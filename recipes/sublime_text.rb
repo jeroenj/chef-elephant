73110dad987e04eae8b76d39621c2b15f5ab98eb
Jeroen Jacobs <git@jeroenj.be>
2013-03-10 16:27:14 +0100

    [sublime_text] Moves settings path into a variable

--- recipes/sublime_text.rb
+++ recipes/sublime_text.rb
@@ -5,25 +5,27 @@ dmg_package 'Sublime Text 2' do
   owner node['elephant']['user']
 end
 
+settings_path = "#{ENV['HOME']}/Library/Application Support/Sublime Text 2"
+
 link '/usr/bin/subl' do
   to '/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl'
 end
 
 ['Preferences.sublime-settings', 'Default (OSX).sublime-keymap'].each do |file|
-  cookbook_file "#{ENV['HOME']}/Library/Application Support/Sublime Text 2/Packages/User/#{file}" do
+  cookbook_file ::File.expand_path("Packages/User/#{file}", settings_path) do
     source "sublime_text/#{file}"
     owner node['elephant']['user']
   end
 end
 
-remote_file "#{ENV['HOME']}/Library/Application Support/Sublime Text 2/Installed Packages/Package Control.sublime-package" do
+remote_file ::File.expand_path("Installed Packages/Package Control.sublime-package", settings_path) do
   source 'http://sublime.wbond.net/Package%20Control.sublime-package'
   owner node['elephant']['user']
   :create_if_missing
 end
 
 node['elephant']['sublime_text']['packages'].each do |package|
-  git ::File.expand_path(package['name'], "#{ENV['HOME']}/Library/Application Support/Sublime Text 2/Packages") do
+  git ::File.expand_path("Packages/#{package['name']}", settings_path) do
     repository package['source']
     user node['elephant']['user']
     action :sync
