405e90d4748fd0e7521f72992cc135f967bddf63
Jeroen Jacobs <git@jeroenj.be>
2013-03-10 15:23:49 +0100

    [Drobo Dashboard] Adds recipe

---
+++ attributes/drobo.rb
@@ -0,0 +1,3 @@
+default['elephant']['drobo']['version'] = '2.4.1'
+default['elephant']['drobo']['checksum'] = '8fdc7b79e8309080041e349531fd065acd6b9faf8dc72b4b2bc563ec45c5ef42'
+default['elephant']['drobo']['url'] = "http://www.drobo.com/support/updates/dashboard/Drobo-Dashboard-installer-#{node['elephant']['drobo']['version']}.dmg"
