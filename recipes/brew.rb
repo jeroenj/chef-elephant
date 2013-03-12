bb6df3427838cbe213e5e1d63a735b11f320ec3c
Jeroen Jacobs <git@jeroenj.be>
2013-03-12 20:31:19 +0100

    [brew] Adds dupes tap

--- recipes/brew.rb
+++ recipes/brew.rb
@@ -1,4 +1,6 @@
-node["elephant"]["homebrew"]["packages"].each do |package|
+homebrew_tap 'homebrew/dupes'
+
+node['elephant']['homebrew']['packages'].each do |package|
   package package do
     action :install
   end
