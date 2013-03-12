7d70be20a382f1df5f7ce9ee0242891e3bdfee46
Jeroen Jacobs <git@jeroenj.be>
2013-03-12 21:05:22 +0100

    [gitx] settings: hide toolbar

--- attributes/gitx.rb
+++ attributes/gitx.rb
@@ -4,5 +4,8 @@ default['mac_os_x']['settings']['gitx'] = {
   'PBShowOpenPanelOnLaunch' => false,
   'PBRefreshAutomatically' => false,
   'gitExecutable' => '/usr/local/bin/git',
-  'SUEnableAutomaticChecks' => true
+  'SUEnableAutomaticChecks' => true,
+  'NSToolbar Configuration A66F2540-5B64-4016-89F7-892563371FAF' => {
+    'TB Is Shown' => 0
+  }
 }
