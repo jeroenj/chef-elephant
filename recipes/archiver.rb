df5c059cf8496ed64397827fc01a253cf72d6565
Jeroen Jacobs <git@jeroenj.be>
2013-03-22 21:53:32 +0100

    [archiver] Removes useless download link

--- recipes/archiver.rb
+++ recipes/archiver.rb
@@ -1,6 +1,3 @@
-'http://storage.googleapis.com/incrediblebee/apps/Archiver/Archiver.zip'
-
-
 remote_file "#{Chef::Config[:file_cache_path]}/Archiver.zip" do
   checksum '7da3c87a788b896a276801a2dee700b5072519d81620648011732d7c9692dbb0'
   source 'http://storage.googleapis.com/incrediblebee/apps/Archiver/Archiver.zip'
