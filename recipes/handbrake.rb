c82bbd7c1fd49a88947d9037a9b2468e362d9d22
Jeroen Jacobs <git@jeroenj.be>
2013-03-22 22:42:14 +0100

    [handbrake] Moves version and checksum to attributes

--- recipes/handbrake.rb
+++ recipes/handbrake.rb
@@ -1,7 +1,6 @@
-dmg_package "Handbrake" do
-  volumes_dir "HandBrake-0.9.8-MacOSX.6_GUI_x86_64"
-  source "http://handbrake.fr/rotation.php?file=HandBrake-0.9.8-MacOSX.6_GUI_x86_64.dmg"
-  checksum "6c1d72d0d305542feb4e2986055a623783bcf77ce2281b2863ccbb8d41204373"
-  owner node["elephant"]["user"]
-  action :install
+dmg_package 'Handbrake' do
+  volumes_dir "HandBrake-#{node['elephant']['handbrake']['version']}-MacOSX.6_GUI_x86_64"
+  source "http://handbrake.fr/rotation.php?file=HandBrake-#{node['elephant']['handbrake']['version']}-MacOSX.6_GUI_x86_64.dmg"
+  checksum node['elephant']['handbrake']['checksum']
+  owner node['elephant']['user']
 end
