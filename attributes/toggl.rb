1ef7fe9aef3d3af8995eca5cfaf237a44ae4d856
Jeroen Jacobs <git@jeroenj.be>
2013-03-22 22:43:28 +0100

    [toggl] Moves version and checksum to attributes

---
+++ attributes/toggl.rb
@@ -0,0 +1,1 @@
+default['elephant']['toggl']['checksum'] = 'b5f91ee4f62d36c0490c021d5fb134b9e7cb3936'
--- recipes/toggl.rb
+++ recipes/toggl.rb
@@ -1,6 +1,6 @@
 dmg_package 'TogglDesktop' do
   volumes_dir 'Toggl Desktop Installer'
   source 'https://download.toggl.com/toggldesktop/latest/TogglDesktopInstaller_Release.dmg'
-  checksum '0c3a4ae06594ff38f201138cef48f885fc6574590033b1a561b9a82989d6c4b2'
+  checksum node['elephant']['toggl']['checksum']
   owner node['elephant']['user']
 end
