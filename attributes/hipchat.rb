4f4f606a110f531ae1b085ae88ded83bd04abdba
Jeroen Jacobs <git@jeroenj.be>
2013-03-26 21:38:19 +0100

    [hipchat] Adds recipe

---
+++ attributes/hipchat.rb
@@ -0,0 +1,10 @@
+default['elephant']['hipchat']['file'] = 'HipChat-0.26-01uennzomg01wmg.zip'
+default['elephant']['hipchat']['checksum'] = 'e44f8bc288f8aac7b453b4fed45ee2b612121468ec6c768a25057fcd21d9d6b4'
+
+default['mac_os_x']['settings']['hipchat'] = {
+  'domain' => 'ws.hipchat.HipChat',
+  'user' => node['elephant']['user'],
+  'notifyForPrivate' => true,
+  'notifyForPrivateRoom' => false,
+  'SUEnableAutomaticChecks' => true,
+}
