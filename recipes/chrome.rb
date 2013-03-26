c24e7edfdafa18d0863b05a21563dded195c99ad
Jeroen Jacobs <git@jeroenj.be>
2013-03-26 22:45:28 +0100

    [chrome] Removes comments from recipe

--- recipes/chrome.rb
+++ recipes/chrome.rb
@@ -4,12 +4,3 @@ dmg_package 'Google Chrome' do
   checksum '67de3a3a1d4a130912c5e31c9e2917585b3ec85b927d1498c89f9a360d4e052b'
   owner node['elephant']['user']
 end
-
-# dmg_package 'beid' do
-#   source "http://eid-mw.googlecode.com/files/beid-#{node['elephant']['beid']['version']}.dmg"
-#   checksum node['elephant']['beid']['checksum']
-#   owner node['elephant']['user']
-#   volumes_dir "beid-#{node['elephant']['beid']['version'].split('-').first}"
-#   type 'pkg'
-#   package_id 'be.eid.middleware'
-# end
