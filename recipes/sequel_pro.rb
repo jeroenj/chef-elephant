f34b7e96319d3435b6a5b6e3a393ef4ee0d32782
Jeroen Jacobs <git@jeroenj.be>
2013-03-28 10:37:55 +0100

    [sequel_pro] Adds recipe

---
+++ recipes/sequel_pro.rb
@@ -0,0 +1,6 @@
+dmg_package 'Sequel Pro' do
+  volumes_dir "Sequel Pro #{node['elephant']['sequel_pro']['version']}"
+  source "http://sequel-pro.googlecode.com/files/sequel-pro-#{node['elephant']['sequel_pro']['version']}.dmg"
+  checksum node['elephant']['sequel_pro']['checksum']
+  owner node['elephant']['user']
+end
