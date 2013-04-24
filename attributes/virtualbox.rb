1c1338c60753a1c1611450102f0928b5c7ca7b2b
Jeroen Jacobs <git@jeroenj.be>
2013-04-24 09:42:20 +0200

    [virtualbox] Updates to v4.2.12

--- attributes/virtualbox.rb
+++ attributes/virtualbox.rb
@@ -1,6 +1,6 @@
-default['elephant']['virtualbox']['version'] = '4.2.10-84104'
+default['elephant']['virtualbox']['version'] = '4.2.12-84980'
 default['elephant']['virtualbox']['url'] = "http://download.virtualbox.org/virtualbox/#{node['elephant']['virtualbox']['version'].split('-').first}/VirtualBox-#{node['elephant']['virtualbox']['version']}-OSX.dmg"
-default['elephant']['virtualbox']['checksum'] = '1c53a4b14c91189f162e841145cc38316957be49a63431286ca901746818fc04'
+default['elephant']['virtualbox']['checksum'] = '75d3e5f1fe264faf937db7e7db12765cd643f6eaac18cbb44801a0108196b32a'
 
 default['mac_os_x']['settings']['virtualbox'] = {
   'domain' => 'ws.agile.1password',
