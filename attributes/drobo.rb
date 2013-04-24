f0e18148620aa2d472446be53e5cfaf4f83a6269
Jeroen Jacobs <git@jeroenj.be>
2013-04-24 10:02:50 +0200

    [drobo] Updates download url

--- attributes/drobo.rb
+++ attributes/drobo.rb
@@ -1,3 +1,3 @@
 default['elephant']['drobo']['version'] = '2.4.3'
 default['elephant']['drobo']['checksum'] = 'e8996617a849685243097ec392eac1a45156d55fa6062504df61c2e2db0fdc8f'
-default['elephant']['drobo']['url'] = "http://www.drobo.com/support/updates/dashboard/Drobo-Dashboard-installer-#{node['elephant']['drobo']['version']}.dmg"
+default['elephant']['drobo']['url'] = "http://www.drobo.com/start/download/dashboard/Drobo-Dashboard-installer-#{node['elephant']['drobo']['version']}.dmg"
