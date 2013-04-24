1066c92a6ee6827f3a642fae5e3fc237e67f922f
Jeroen Jacobs <git@jeroenj.be>
2013-04-24 09:41:16 +0200

    [drobo] Updates to v2.4.3

--- attributes/drobo.rb
+++ attributes/drobo.rb
@@ -1,3 +1,3 @@
-default['elephant']['drobo']['version'] = '2.4.1'
-default['elephant']['drobo']['checksum'] = '8fdc7b79e8309080041e349531fd065acd6b9faf8dc72b4b2bc563ec45c5ef42'
+default['elephant']['drobo']['version'] = '2.4.3'
+default['elephant']['drobo']['checksum'] = 'e8996617a849685243097ec392eac1a45156d55fa6062504df61c2e2db0fdc8f'
 default['elephant']['drobo']['url'] = "http://www.drobo.com/support/updates/dashboard/Drobo-Dashboard-installer-#{node['elephant']['drobo']['version']}.dmg"
