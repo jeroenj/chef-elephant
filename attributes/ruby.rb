e15c311ad12ad2946eedd3b92af6a97fa9d65ac3
Jeroen Jacobs <git@jeroenj.be>
2013-05-23 22:42:50 +0200

    [ruby] Adds chruby to manage rubies. ruby-build to install rubies

---
+++ attributes/ruby.rb
@@ -0,0 +1,9 @@
+default[:elephant][:ruby] = {
+  :path => '/usr/local/var/rubies',
+  :rubies => %w[
+    2.0.0-p195
+    1.9.3-p429
+    1.9.3-p392-railsexpress
+  ],
+  :default => '2.0.0'
+}
--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -1,3 +1,21 @@
+package 'chruby'
+package 'ruby-build'
+
+node[:elephant][:ruby][:rubies].each do |ruby|
+  path = ::File.join node[:elephant][:ruby][:path], "ruby-#{ruby}"
+
+  ruby_build_ruby ruby do
+    definition(File.expand_path(File.join(__FILE__, "../../files/default/ruby/ruby_railsexpress"))) if ruby =~ /.*-railsexpress/
+    prefix_path path
+  end
+end
+
+if node[:recipes].include?('elephant::oh_my_zsh')
+  template "#{ENV['HOME']}/.oh-my-zsh/custom/chruby.zsh" do
+    source 'ruby/chruby.zsh.erb'
+  end
+end
+
 %w[gemrc].each do |file|
   cookbook_file "#{ENV['HOME']}/.#{file}" do
     source "ruby/#{file}"
