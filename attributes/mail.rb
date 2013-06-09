21b0d985e19e7ac7ca87378e04e930ea824c7855
Jeroen Jacobs <git@jeroenj.be>
2013-06-09 16:30:34 +0200

    [mail] Moves mail preferences to separate recipe

---
+++ attributes/mail.rb
@@ -0,0 +1,13 @@
+default[:mac_os_x][:settings][:mail] = {
+  'domain' => '/Users/jeroen/Library/Containers/com.apple.mail/Data/Library/Preferences/com.apple.mail.plist',
+  'AutoReplyFormat' => true,
+  'EnableContactPhotos' => true,
+  'EnableToCCInMessageList' => true,
+  'IndexJunk' => true,
+  'JunkMailBehavior' => 2,
+  'NumberOfSnippetLines' => 0,
+  'PollTime' => 1,
+  'ShouldShowUnreadMessagesInBold' => true,
+  'SuppressDeliveryFailure' => 1,
+  'ThreadingDefault' => false
+}
--- attributes/preferences.rb
+++ attributes/preferences.rb
@@ -24,20 +24,6 @@ default[:mac_os_x][:settings][:finder] = {
   'WarnOnEmptyTrash' => false
 }
 
-default[:mac_os_x][:settings][:mail] = {
-  'domain' => '/Users/jeroen/Library/Containers/com.apple.mail/Data/Library/Preferences/com.apple.mail.plist',
-  'AutoReplyFormat' => true,
-  'EnableContactPhotos' => true,
-  'EnableToCCInMessageList' => true,
-  'IndexJunk' => true,
-  'JunkMailBehavior' => 2,
-  'NumberOfSnippetLines' => 0,
-  'PollTime' => 1,
-  'ShouldShowUnreadMessagesInBold' => true,
-  'SuppressDeliveryFailure' => 1,
-  'ThreadingDefault' => false
-}
-
 default[:mac_os_x][:settings][:safari] = {
   'domain' => 'com.apple.safari',
   'AutoFillPasswords' => false,
