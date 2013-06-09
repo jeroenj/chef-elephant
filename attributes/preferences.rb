82a61d23110cd4cb542bad65fd7383c63796ebad
Jeroen Jacobs <git@jeroenj.be>
2013-06-09 16:31:37 +0200

    [safari] Moves safari preferences to separate recipe

--- attributes/preferences.rb
+++ attributes/preferences.rb
@@ -24,19 +24,6 @@ default[:mac_os_x][:settings][:finder] = {
   'WarnOnEmptyTrash' => false
 }
 
-default[:mac_os_x][:settings][:safari] = {
-  'domain' => 'com.apple.safari',
-  'AutoFillPasswords' => false,
-  'AutoOpenSafeDownloads' => false,
-  'DownloadsClearingPolicy' => 1,
-  'HistoryAgeInDaysLimit' => 365000,
-  'HomePage' => '',
-  'IncludeDevelopMenu' => true,
-  'NewWindowBehavior' => 4,
-  'SendDoNotTrackHTTPHeader' => true,
-  'TabCreationPolicy' => 2
-}
-
 default[:mac_os_x][:settings][:screensaver] = {
   'domain' => 'com.apple.screensaver',
   'askForPasswordDelay' => 3600
---
+++ attributes/safari.rb
@@ -0,0 +1,12 @@
+default[:mac_os_x][:settings][:safari] = {
+  'domain' => 'com.apple.safari',
+  'AutoFillPasswords' => false,
+  'AutoOpenSafeDownloads' => false,
+  'DownloadsClearingPolicy' => 1,
+  'HistoryAgeInDaysLimit' => 365000,
+  'HomePage' => '',
+  'IncludeDevelopMenu' => true,
+  'NewWindowBehavior' => 4,
+  'SendDoNotTrackHTTPHeader' => true,
+  'TabCreationPolicy' => 2
+}
