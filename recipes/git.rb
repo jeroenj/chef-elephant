dc5ba007dd7c32b7cd5d55d9ed974dc1352122cd
Jeroen Jacobs <git@jeroenj.be>
2013-06-10 12:24:11 +0200

    [git] Moves gitignore to files instead of templates

--- recipes/git.rb
+++ recipes/git.rb
@@ -1,5 +1,9 @@
-%w[gitconfig gitignore githelpers].each do |file|
+%w[gitconfig githelpers].each do |file|
   template "#{ENV['HOME']}/.#{file}" do
     source "git/#{file}.erb"
   end
+end
+
+cookbook_file "#{ENV['HOME']}/.gitignore" do
+  source 'git/gitignore'
 end
