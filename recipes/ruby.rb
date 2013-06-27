24a1b2b81dbebdd316c560f8ba0a62cb13f61dbe
Jeroen Jacobs <git@jeroenj.be>
2013-06-27 17:38:24 +0200

    [ruby] Cleanup sources after installing a ruby

--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -2,7 +2,8 @@ package 'chruby'
 package 'ruby-install'
 
 node[:elephant][:ruby][:rubies].each do |ruby|
-  path = ::File.join node[:elephant][:ruby][:path], "ruby-#{ruby}"
+  name = "ruby-#{ruby}"
+  path = ::File.join node[:elephant][:ruby][:path], name
 
   cmd = "ruby-install --install-dir #{path} ruby #{ruby}"
   cmd += " -- --without-tk" if ruby =~ /.*1\.8\.7.*/
@@ -10,6 +11,21 @@ node[:elephant][:ruby][:rubies].each do |ruby|
   execute cmd do
     not_if{::File.exists? path}
   end
+
+  file "#{ENV['HOME']}/src/#{name}.tar.bz2" do
+    action :delete
+  end
+
+  directory "#{ENV['HOME']}/src/#{name}" do
+    recursive true
+    action :delete
+  end
+end
+
+directory "#{ENV['HOME']}/src" do
+  action :delete
+  recursive true
+  only_if{Dir["#{ENV['HOME']}/src/*"].empty?}
 end
 
 if node[:recipes].include?('elephant::oh_my_zsh')
