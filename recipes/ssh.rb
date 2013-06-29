d26829668916272623793b6d15c08c90e74ac6c0
Jeroen Jacobs <git@jeroenj.be>
2013-06-29 13:02:39 +0200

    [ssh] Supports private/public keys

--- recipes/ssh.rb
+++ recipes/ssh.rb
@@ -1,3 +1,18 @@
 template "#{ENV['HOME']}/.ssh/config" do
   source 'ssh/config.erb'
 end
+
+private_key = node[:elephant][:ssh][:private_key]
+public_key = node[:elephant][:ssh][:private_key]
+
+if private_key && public_key
+  file "#{ENV['HOME']}/.ssh/id_rsa" do
+    content private_key
+    mode 00600
+  end
+
+  file "#{ENV['HOME']}/.ssh/id_rsa.pub" do
+    content public_key
+    mode 00644
+  end
+end
