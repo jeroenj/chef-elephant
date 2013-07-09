38c149ba18f84ea7439d6bd5febbc811405ffc34
Jeroen Jacobs <git@jeroenj.be>
2013-07-09 16:49:39 +0200

    [divvy] Adds recipe: preferences

---
+++ attributes/divvy.rb
@@ -0,0 +1,14 @@
+default[:elephant][:settings][:divvy] = {
+  'domain' => 'com.mizage.Divvy',
+  'defaultColumnCount' => 10,
+  'defaultRowCount' => 10,
+  'enableAcceleration' => true,
+  'globalHotkey' => {
+    'keyCode' => 49,
+    'modifiers' => 768
+  },
+  'showMenuIcon' => false,
+  'useDefaultGrid' => true,
+  'useGlobalHotkey' => true,
+  'useMonitorCycling' => true
+}
