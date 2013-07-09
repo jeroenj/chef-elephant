90bbaec606a03bbd2047dabf4c766cdfba60a44b
Jeroen Jacobs <git@jeroenj.be>
2013-07-09 14:15:29 +0200

    [ruby] Updates rubies to install

--- attributes/ruby.rb
+++ attributes/ruby.rb
@@ -1,9 +1,9 @@
 default[:elephant][:ruby] = {
   :path => '/usr/local/var/rubies',
   :rubies => %w[
-    2.0.0-p195
-    1.9.3-p429
-    1.8.7-p371
+    2.0.0-p247
+    1.9.3-p448
+    1.8.7-p374
   ],
   :default => '2.0.0'
 }
