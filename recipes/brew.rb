191887b1db619df0eabf4c3f64b7c463d4151d9f
Jeroen Jacobs <git@jeroenj.be>
2013-07-09 14:17:09 +0200

    [brew] Symlink mariadb launch agent if mariadb is in packages list

--- recipes/brew.rb
+++ recipes/brew.rb
@@ -1,5 +1,13 @@
 homebrew_tap 'homebrew/dupes'
 
-node[:elephant][:homebrew][:packages].each do |pkg|
+packages = node[:elephant][:homebrew][:packages]
+
+packages.each do |pkg|
   package pkg
+end
+
+if packages.include?('mariadb')
+  link "#{ENV['HOME']}/Library/LaunchAgents/homebrew.mxcl.mariadb.plist" do
+    to '/usr/local/opt/mariadb/homebrew.mxcl.mariadb.plist'
+  end
 end
