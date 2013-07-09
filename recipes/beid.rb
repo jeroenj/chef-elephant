bc6499aac43f4c94e34ede53db0884a00018c994
Jeroen Jacobs <git@jeroenj.be>
2013-07-09 14:16:17 +0200

    [beid] Uses correct app name in dmg

--- recipes/beid.rb
+++ recipes/beid.rb
@@ -1,6 +1,7 @@
 dmg_package 'beid' do
   source "http://eid-mw.googlecode.com/files/beid-#{node[:elephant][:beid][:version]}.dmg"
   checksum node[:elephant][:beid][:checksum]
+  app "beid-#{node[:elephant][:beid][:version].split('-').last}"
   volumes_dir "beid-#{node[:elephant][:beid][:version].split('-').first}"
   type 'pkg'
   package_id 'be.eid.middleware'
