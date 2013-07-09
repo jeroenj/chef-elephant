a5e3173f1927bbc32b40adf237844f3eacab4f44
Jeroen Jacobs <git@jeroenj.be>
2013-07-09 10:51:20 +0200

    [pow] Create and symlink hosts directory since Homebrew does not do this (yet)

--- recipes/pow.rb
+++ recipes/pow.rb
@@ -3,6 +3,16 @@ pow_installed = ::File.exists? '/usr/local/bin/pow'
 package 'pow'
 
 unless pow_installed
+  # Homebrew does not take care of this (yet)
+  # https://github.com/37signals/pow/issues/298
+  pow_hosts = "#{ENV['HOME']}/Library/Application Support/Pow/Hosts"
+  directory pow_hosts do
+    recursive true
+  end
+  link "#{ENV['HOME']}/.pow" do
+    to pow_hosts
+  end
+
   execute 'sudo pow --install-system'
   execute 'pow --install-local'
   execute 'sudo launchctl load -w /Library/LaunchDaemons/cx.pow.firewall.plist'
