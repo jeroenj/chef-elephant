7208908e16e48d1b7a353a53dc05a7464ed6da95
Jeroen Jacobs <git@jeroenj.be>
2013-07-09 14:23:29 +0200

    [git] Only add git oh-my-zsh plugin if oh-my-zsh is in the run list

--- recipes/git.rb
+++ recipes/git.rb
@@ -2,8 +2,10 @@ cookbook_file "#{ENV['HOME']}/.gitignore" do
   source 'git/gitignore'
 end
 
-cookbook_file "#{ENV['HOME']}/.oh-my-zsh/custom/git.zsh" do
-  source 'git/git.zsh'
+if node[:recipes].include?('elephant::oh_my_zsh')
+  cookbook_file "#{ENV['HOME']}/.oh-my-zsh/custom/git.zsh" do
+    source 'git/git.zsh'
+  end
 end
 
 template "#{ENV['HOME']}/.gitconfig" do
