58098797ff83e47428b946d353cf3f66420d98e6
Jeroen Jacobs <git@jeroenj.be>
2013-07-09 14:14:41 +0200

    [java] Updates to 7 Update 25

--- attributes/java.rb
+++ attributes/java.rb
@@ -1,3 +1,3 @@
-default[:elephant][:java][:version] = '7 Update 17'
-default[:elephant][:java][:url] = 'http://javadl.sun.com/webapps/download/AutoDL?BundleId=75253'
-default[:elephant][:java][:checksum] = '7515f1296b5b9fd48db0cfcb65ccffc218f5d8ec5a6074b2acba38a3230e87be'
+default[:elephant][:java][:version] = '7 Update 25'
+default[:elephant][:java][:url] = 'http://javadl.sun.com/webapps/download/AutoDL?BundleId=78698'
+default[:elephant][:java][:checksum] = 'b89996e10f833187d56cd7a740934dbca8232a7337e0250163f82039bacf7324'
