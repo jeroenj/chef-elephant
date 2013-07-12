b9f5fd11d3dc0fe68bca5a7538974596a598c4b4
Jeroen Jacobs <git@jeroenj.be>
2013-07-12 15:04:03 +0200

    [skype] Disable Skype notification sounds

--- attributes/skype.rb
+++ attributes/skype.rb
@@ -14,5 +14,61 @@ default[:elephant][:settings][:skype] = {
   'SKShowWelcomeTour' => false,
   'SKSilentUpgradeNoticeShown' => true,
   'SKUseGrowlInsteadOfDefault' => true,
-  'SourceListLayoutMode' => false
+  'SourceListLayoutMode' => false,
+  'UserDefinedEvents' => '{' \
+                         '  Busy = {' \
+                         '    PlaySound = 0;' \
+                         '  };' \
+                         '  CallConnecting = {' \
+                         '    PlaySound = 0;' \
+                         '  };' \
+                         '  ContactBecomesAvailable = {' \
+                         '    PlaySound = 0;' \
+                         '  };' \
+                         '  ContactRequestReceived = {' \
+                         '    PlaySound = 0;' \
+                         '  };' \
+                         '  ContactsReceived = {' \
+                         '    PlaySound = 0;' \
+                         '  };' \
+                         '  FileTransferCompleted = {' \
+                         '    PlaySound = 0;' \
+                         '  };' \
+                         '  FileTransferFailed = {' \
+                         '    PlaySound = 0;' \
+                         '  };' \
+                         '  FirstMessageReceived = {' \
+                         '    PlaySound = 0;' \
+                         '  };' \
+                         '  HangUp = {' \
+                         '    PlaySound = 0;' \
+                         '  };' \
+                         '  HoldCall = {' \
+                         '    PlaySound = 0;' \
+                         '  };' \
+                         '  IncomingCallWaiting = {' \
+                         '    PlaySound = 0;' \
+                         '  };' \
+                         '  IncomingFileTransfer = {' \
+                         '    PlaySound = 0;' \
+                         '  };' \
+                         '  MessageReceived = {' \
+                         '    PlaySound = 0;' \
+                         '  };' \
+                         '  MessageSent = {' \
+                         '    PlaySound = 0;' \
+                         '  };' \
+                         '  OutgoingCall = {' \
+                         '    PlaySound = 0;' \
+                         '  };' \
+                         '  RemotePartyHungUp = {' \
+                         '    PlaySound = 0;' \
+                         '  };' \
+                         '  ResumeCall = {' \
+                         '    PlaySound = 0;' \
+                         '  };' \
+                         '  SignIn = {' \
+                         '    PlaySound = 0;' \
+                         '  };' \
+                         '}'
 }
