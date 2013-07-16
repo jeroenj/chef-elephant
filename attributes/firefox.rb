5fe196d1eef5ea16ca223aab47752c446f7a2271
Jeroen Jacobs <git@jeroenj.be>
2013-07-16 13:44:38 +0200

    [firefox] Adds recipe

---
+++ attributes/firefox.rb
@@ -0,0 +1,3 @@
+default[:elephant][:firefox][:version] = '22.0'
+default[:elephant][:firefox][:url] = "http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/#{node[:elephant][:firefox][:version]}/mac/en-US/Firefox%20#{node[:elephant][:firefox][:version]}.dmg"
+default[:elephant][:firefox][:checksum] = '45ef69d4254c725114c3fa8ffc5e5bef331e46bd'
