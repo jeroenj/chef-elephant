7bcec8b108ae6c89c6891dae01036f716e3aae4e
Jeroen Jacobs <git@jeroenj.be>
2013-07-16 13:45:30 +0200

    [firefox] Removes some copied comments from recipe

--- recipes/firefox.rb
+++ recipes/firefox.rb
@@ -1,8 +1,4 @@
 dmg_package 'Firefox' do
   source node[:elephant][:firefox][:url]
   checksum node[:elephant][:firefox][:checksum]
-  # app "beid-#{node[:elephant][:beid][:version].split('-').last}"
-  # volumes_dir "beid-#{node[:elephant][:beid][:version].split('-').first}"
-  # type 'pkg'
-  # package_id 'be.eid.middleware'
 end
