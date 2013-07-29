a136f31fd6436cab8fbe4545237856577028b527
Jeroen Jacobs <git@jeroenj.be>
2013-07-29 13:23:31 +0200

    [sequel_pro] Moves download url to attributes

--- attributes/sequel_pro.rb
+++ attributes/sequel_pro.rb
@@ -1,4 +1,5 @@
 default[:elephant][:sequel_pro][:version] = '1.0.1'
+default[:elephant][:sequel_pro][:url] = "http://sequel-pro.googlecode.com/files/sequel-pro-#{node[:elephant][:sequel_pro][:version]}.dmg"
 default[:elephant][:sequel_pro][:checksum] = '256873df2bdaebad9997acb2182428790454cb577d2c1af9565ecc4f670ebf92'
 
 default[:elephant][:settings][:sequel_pro] = {
--- recipes/sequel_pro.rb
+++ recipes/sequel_pro.rb
@@ -2,6 +2,6 @@
 
 dmg_package 'Sequel Pro' do
   volumes_dir "Sequel Pro #{node[:elephant][:sequel_pro][:version]}"
-  source "http://sequel-pro.googlecode.com/files/sequel-pro-#{node[:elephant][:sequel_pro][:version]}.dmg"
+  source node[:elephant][:sequel_pro][:url]
   checksum node[:elephant][:sequel_pro][:checksum]
 end
