c22f5776547bd3056d577fba42958ab5e2d1232e
Jeroen Jacobs <git@jeroenj.be>
2013-07-29 13:22:44 +0200

    [jubler] Moves download url to attributes

--- attributes/jubler.rb
+++ attributes/jubler.rb
@@ -1,2 +1,3 @@
 default[:elephant][:jubler][:version] = '4.6.1'
+default[:elephant][:jubler][:url] = "http://jubler.googlecode.com/files/Jubler-#{node[:elephant][:jubler][:version]}.dmg"
 default[:elephant][:jubler][:checksum] = '7a5d08d950ccd7869fedb25f6d0ed8b8e8277e99a9032601de641b3de6545a13'
--- recipes/jubler.rb
+++ recipes/jubler.rb
@@ -1,4 +1,4 @@
 dmg_package 'Jubler' do
-  source "http://jubler.googlecode.com/files/Jubler-#{node[:elephant][:jubler][:version]}.dmg"
+  source node[:elephant][:jubler][:url]
   checksum node[:elephant][:vlc][:checksum]
 end
