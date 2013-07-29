7de9ca6f858e22feca9b1568e0a8a12da88f68ca
Jeroen Jacobs <git@jeroenj.be>
2013-07-29 13:28:35 +0200

    [vlc] Moves download url to attributes

--- recipes/vlc.rb
+++ recipes/vlc.rb
@@ -1,5 +1,5 @@
 dmg_package 'VLC' do
-  source "http://download.videolan.org/vlc/#{node[:elephant][:vlc][:version]}/macosx/vlc-#{node[:elephant][:vlc][:version]}.dmg"
+  source node[:elephant][:vlc][:url]
   checksum node[:elephant][:vlc][:checksum]
   dmg_name "vlc-#{node[:elephant][:vlc][:version]}"
   volumes_dir "vlc-#{node[:elephant][:vlc][:version]}"
