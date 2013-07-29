73dc9f76e57210e74fe7a7a8acf134cf685cb8e6
Jeroen Jacobs <git@jeroenj.be>
2013-07-29 13:30:47 +0200

    [chrome] Moves download url and checksum to attributes

---
+++ attributes/chrome.rb
@@ -0,0 +1,2 @@
+default[:elephant][:chrome][:url] = 'https://dl.google.com/chrome/mac/stable/GGRM/googlechrome.dmg'
+default[:elephant][:chrome][:checksum] = '67de3a3a1d4a130912c5e31c9e2917585b3ec85b927d1498c89f9a360d4e052b'
