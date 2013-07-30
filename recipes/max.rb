2867cf5d6263005dd98bda3fd77090ea44e4626e
Jeroen Jacobs <git@jeroenj.be>
2013-07-30 19:15:36 +0200

    [max] Fixes description

--- recipes/max.rb
+++ recipes/max.rb
@@ -6,7 +6,7 @@ remote_file "#{Chef::Config[:file_cache_path]}/max_#{node[:elephant][:max][:vers
   not_if { ::File.directory?('/Applications/Max.app') }
 end
 
-execute 'Install Alfred' do
+execute 'Install Max' do
   command "tar -jxf #{Chef::Config[:file_cache_path]}/max_#{node[:elephant][:max][:version]}.zip"
   cwd '/Applications'
   not_if { ::File.directory?('/Applications/Max.app') }
