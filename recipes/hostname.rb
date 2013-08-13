661bad494e451aa67df243fff9d4cae6db78c076
Jeroen Jacobs <git@jeroenj.be>
2013-08-13 09:59:19 +0200

    [hostname] Cleanup recipe. Only try to set it if it's different from the current value

--- recipes/hostname.rb
+++ recipes/hostname.rb
@@ -1,22 +1,6 @@
-hostname = node[:elephant][:hostname]
-
-log "My hostname: #{hostname}"
-
-# The scutil commands need to run as root, unless
-# you're logged into the console, but we can't be sure of that.
-
-[
-  "scutil --set ComputerName #{hostname}",
-  "scutil --set LocalHostName #{hostname}",
-  "scutil --set HostName #{hostname}",
-  "hostname #{hostname}",
-  "diskutil rename / #{hostname}"
-].each do |command|
-  execute command
-end
-
-ruby_block "test to see if hostname was set" do
-  block do
-    raise "Setting of hostname failed" unless hostname == `scutil --get ComputerName`.chop
+%w[ComputerName LocalHostName HostName].each do |type|
+  execute "Set #{type} to #{node[:elephant][:hostname]}" do
+    command "scutil --set #{type} #{node[:elephant][:hostname]}"
+    not_if { `scutil --get #{type}`.strip == node[:elephant][:hostname]}
   end
 end
