49f3ae12e0d6431dd26cb4b789ecf9e9170a2933
Jeroen Jacobs <git@jeroenj.be>
2013-08-13 10:05:33 +0200

    [oh_my_zsh] Remove update-global-gem() function

--- files/default/oh_my_zsh/ruby.zsh
+++ files/default/oh_my_zsh/ruby.zsh
@@ -1,13 +1,3 @@
-function update-global-gem() {
-  name=$1
-  rubies=($(rvm list strings))
-  for ruby in $rubies; do
-    rvm use $ruby@global
-    gem uninstall $name --all --ignore-dependencies --executables
-    gem install $name
-  done
-}
-
 function ruby-version() {
   ruby -e 'puts "#{RUBY_VERSION}p#{RUBY_PATCHLEVEL}"'
 }
