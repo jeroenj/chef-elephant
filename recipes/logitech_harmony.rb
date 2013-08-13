dc18091866d712f5e197e2c5d6669152eb8975a3
Jeroen Jacobs <git@jeroenj.be>
2013-08-13 09:46:59 +0200

    [logitech_harmony] Set correct package_id

--- recipes/logitech_harmony.rb
+++ recipes/logitech_harmony.rb
@@ -2,5 +2,5 @@ dmg_package 'LogitechRemoteSoftware' do
   source node[:elephant][:logitech_harmony][:url]
   checksum node[:elephant][:logitech_harmony][:checksum]
   type 'pkg'
-  package_id 'com.logitech.harmony'
+  package_id 'com.logitech.harmony.logitechRemoteSoftware.LogitechHarmonyRemoteSoftware.pkg'
 end
