b887a345a62de35872f141e1be6b375ae7e54576
Jeroen Jacobs <git@jeroenj.be>
2013-08-14 12:59:40 +0200

    [virtualbox] Updates checksum for v4.2.16

--- attributes/virtualbox.rb
+++ attributes/virtualbox.rb
@@ -1,6 +1,6 @@
 default[:elephant][:virtualbox][:version] = '4.2.16-86992'
 default[:elephant][:virtualbox][:url] = "http://download.virtualbox.org/virtualbox/#{node[:elephant][:virtualbox][:version].split('-').first}/VirtualBox-#{node[:elephant][:virtualbox][:version]}-OSX.dmg"
-default[:elephant][:virtualbox][:checksum] = '75d3e5f1fe264faf937db7e7db12765cd643f6eaac18cbb44801a0108196b32a'
+default[:elephant][:virtualbox][:checksum] = 'be67d3b62aea6734f6003ea3e25e07c05d267e8289df379c27493dd0320ac7bb'
 
 default[:elephant][:settings][:virtualbox] = {
   'domain' => 'org.virtualbox.app.VirtualBox',
