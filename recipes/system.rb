7f74601da6977ad0cb433ad9bde226d25d81b3da
Jeroen Jacobs <git@jeroenj.be>
2013-08-14 22:48:45 +0200

    [system] Only run chflags if it is needed

--- recipes/system.rb
+++ recipes/system.rb
@@ -1,7 +1,8 @@
 include_recipe 'mac_os_x::settings'
 
 execute 'Show library' do
-  command 'chflags nohidden ~/Library/'
+  command "chflags nohidden #{ENV['HOME']}/Library"
+  only_if { `stat #{ENV['HOME']}/Library` =~ /0x8000 #{ENV['HOME'].gsub /\//, '\/'}\/Library/}
 end
 
 if node[:mac_os_x][:settings][:dock]
