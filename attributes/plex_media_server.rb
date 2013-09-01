df06eb17c8da757bc33d80c3d3d988867ecc4762
Jeroen Jacobs <git@jeroenj.be>
2013-09-01 19:51:55 +0200

    Updates to v0.9.8.5

--- attributes/plex_media_server.rb
+++ attributes/plex_media_server.rb
@@ -1,3 +1,3 @@
-default[:elephant][:plex_media_server][:version] = '0.9.8.4.125-ffe2a5d'
+default[:elephant][:plex_media_server][:version] = '0.9.8.5.165-c301c50'
 default[:elephant][:plex_media_server][:url] = "http://downloads.plexapp.com/plex-media-server/#{node[:elephant][:plex_media_server][:version]}/PlexMediaServer-#{node[:elephant][:plex_media_server][:version]}-OSX.dmg"
-default[:elephant][:plex_media_server][:checksum] = 'b24af8d1448c7f93be401e688f825282a0bfb125'
+default[:elephant][:plex_media_server][:checksum] = '5c0d497990f60ee60507c8a08e8332782052c3ae'
