dbc0f99c2d306dfe7c9a8a6dd7a5da2d57c2e842
Jeroen Jacobs <git@jeroenj.be>
2013-09-03 19:59:22 +0200

    [max] Sets domain for settings

--- attributes/max.rb
+++ attributes/max.rb
@@ -3,6 +3,7 @@ default[:elephant][:max][:url] = "http://files.sbooth.org/Max-#{node[:elephant][
 default[:elephant][:max][:checksum] = '6943a30c102bf815c89002671f0f0fd64c1d2805'
 
 default[:elephant][:settings][:max] = {
+  'domain' => 'org.sbooth.Max',
   'SUCheckAtStartup' => true,
   'convertInPlace' => true
 }
