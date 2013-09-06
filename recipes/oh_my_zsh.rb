668ee5ed4226f567b045e5da3c74bed3172e2108
Jeroen Jacobs <git@jeroenj.be>
2013-09-06 14:49:49 +0200

    [oh_my_zsh] Only export VISUAL and EDITOR to Sublime Text when it is available

--- recipes/oh_my_zsh.rb
+++ recipes/oh_my_zsh.rb
@@ -11,14 +11,16 @@ template "#{ENV['HOME']}/.oh-my-zsh/themes/jeroen.zsh-theme" do
   source 'oh_my_zsh/jeroen.zsh-theme.erb'
 end
 
-%w[aliases exports rails ruby].each do |file|
+%w[aliases rails ruby].each do |file|
   cookbook_file "#{ENV['HOME']}/.oh-my-zsh/custom/#{file}.zsh" do
     source "oh_my_zsh/#{file}.zsh"
   end
 end
 
-template "#{ENV['HOME']}/.oh-my-zsh/custom/projects.zsh" do
-  source 'oh_my_zsh/projects.zsh.erb'
+%w[projects exports].each do |file|
+  template "#{ENV['HOME']}/.oh-my-zsh/custom/#{file}.zsh" do
+    source "oh_my_zsh/#{file}.zsh.erb"
+  end
 end
 
 execute 'Set default shell to zsh' do
