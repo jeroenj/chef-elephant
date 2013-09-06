580e743a97acb843824f964d53a4394e9d0ee968
Jeroen Jacobs <git@jeroenj.be>
2013-09-06 20:23:08 +0200

    [data_bags] Adds recipe: converts data bags into node attributes

---
+++ recipes/data_bags.rb
@@ -0,0 +1,28 @@
+names = ['shared', node[:elephant][:hostname].downcase]
+
+# Code copied from ActiveSupport
+# https://github.com/rails/rails/blob/master/activesupport/lib/active_support/core_ext/hash/deep_merge.rb
+class ::Hash
+  def deep_merge(other_hash, &block)
+    dup.deep_merge!(other_hash, &block)
+  end
+
+  # Same as +deep_merge+, but modifies +self+.
+  def deep_merge!(other_hash, &block)
+    other_hash.each_pair do |k,v|
+      tv = self[k]
+      if tv.is_a?(Hash) && v.is_a?(Hash)
+        self[k] = tv.deep_merge(v, &block)
+      else
+        self[k] = block && tv ? block.call(k, tv, v) : v
+      end
+    end
+    self
+  end
+end
+
+names.each do |name|
+  data_bag(name).each do |bag|
+    node.default[:elephant] = node[:elephant].deep_merge data_bag_item(name, bag).raw_data.reject { |k, v| k == 'id' }
+  end
+end
