b5d12a9738661c66c55fbffa39789c76b832548e
Jeroen Jacobs <git@jeroenj.be>
2013-09-10 13:00:17 +0200

    [alfred] Formatting in attributes

--- attributes/alfred.rb
+++ attributes/alfred.rb
@@ -16,5 +16,5 @@ default[:elephant][:settings][:alfred] = {
     '~/Library/Mobile Documents/',
     '~/Library/PreferencePanes'
   ],
-  'syncfolder' =>'~/Dropbox/Preferences'
+  'syncfolder' => '~/Dropbox/Preferences'
 }
