39b32e10e690d00289eab1153aa54bd3d071bf05
Jeroen Jacobs <git@jeroenj.be>
2013-09-10 15:52:39 +0200

    [owncloud] Updates to v1.4.0

--- attributes/owncloud.rb
+++ attributes/owncloud.rb
@@ -1,3 +1,3 @@
-default[:elephant][:owncloud][:version] = '1.3.0'
+default[:elephant][:owncloud][:version] = '1.4.0'
 default[:elephant][:owncloud][:url] = "http://download.owncloud.com/download/ownCloud-#{node[:elephant][:owncloud][:version]}.dmg"
-default[:elephant][:owncloud][:checksum] = '12aff171259a13d6b4f415682688d10ca6507be2'
+default[:elephant][:owncloud][:checksum] = '6246d806509a71233944c0107c720666c3a16799'
