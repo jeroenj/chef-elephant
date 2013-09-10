e2dddbf7eba8540229d4dac6ec7b90ec4d0f05e1
Jeroen Jacobs <git@jeroenj.be>
2013-09-10 16:16:27 +0200

    [owncloud] Ads preferences

--- recipes/owncloud.rb
+++ recipes/owncloud.rb
@@ -2,3 +2,16 @@ dmg_package 'ownCloud' do
   source node[:elephant][:owncloud][:url]
   checksum node[:elephant][:owncloud][:checksum]
 end
+
+settings_path = "#{ENV['HOME']}/Library/Application Support/ownCloud"
+
+directory settings_path
+
+template ::File.expand_path('owncloud.cfg', settings_path) do
+  source 'owncloud/owncloud.cfg.erb'
+  variables({
+    :certificate => node[:elephant][:owncloud][:certificate],
+    :url => node[:elephant][:owncloud][:server_url],
+    :user => node[:elephant][:owncloud][:user]
+  })
+end
