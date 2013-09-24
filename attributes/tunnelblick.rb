58799a296d8a7a3c1d26d0daf3abb030d35d3dbd
Jeroen Jacobs <git@jeroenj.be>
2013-09-24 16:15:32 +0200

    [tunnelblick] Updaets to v3.3

--- attributes/tunnelblick.rb
+++ attributes/tunnelblick.rb
@@ -1,3 +1,3 @@
-default[:elephant][:tunnelblick][:version] = '3.2.8'
-default[:elephant][:tunnelblick][:url] = "http://tunnelblick.googlecode.com/files/Tunnelblick_#{node[:elephant][:tunnelblick][:version]}.dmg"
-default[:elephant][:tunnelblick][:checksum] = 'e3d6ba702b6bc58a1a470d2fcaea95b7ead8e26965820cba2b919d58e9190b78'
+default[:elephant][:tunnelblick][:version] = '3.3'
+default[:elephant][:tunnelblick][:url] = "http://sourceforge.net/projects/tunnelblick/files/All%20files/Tunnelblick_#{node[:elephant][:tunnelblick][:version]}.dmg/download"
+default[:elephant][:tunnelblick][:checksum] = 'c950430cc9a21f6109ff44e8509636d8b4c82cf9'
