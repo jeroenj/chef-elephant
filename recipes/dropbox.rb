4523b8637d82029c2b5d04211707c4aafeadbbac
Jeroen Jacobs <git@jeroenj.be>
2013-10-02 13:34:04 +0200

    [dropbox] Install Dropbox (v2.4.0)

--- recipes/dropbox.rb
+++ recipes/dropbox.rb
@@ -1,3 +1,9 @@
+dmg_package 'Dropbox' do
+  source node[:elephant][:dropbox][:url]
+  checksum node[:elephant][:dropbox][:checksum]
+  volumes_dir 'Dropbox Installer'
+end
+
 link "#{ENV['HOME']}/Development" do
   to "#{ENV['HOME']}/Dropbox/Development"
 end
