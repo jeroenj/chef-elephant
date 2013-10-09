0973c43bd8437d57cdeb5d2e07743d2c1faf3e01
Jeroen Jacobs <git@jeroenj.be>
2013-10-09 13:52:10 +0200

    [sublime_text] Marks Procfile.dev as Ruby

--- attributes/sublime_text.rb
+++ attributes/sublime_text.rb
@@ -4,38 +4,39 @@ default[:elephant][:sublime_text][:checksum] = '14fba173566d4415f1881a38ad98cf22
 
 default[:elephant][:sublime_text] = {
   :extensions => {
-    'Ruby' => %w[Cheffile Vagrantfile],
+    'Ruby' => %w[Cheffile Vagrantfile Procfile.dev],
     'Sass' => %w[sass],
     'SCSS' => %w[scss],
     'XML' => %w[xml xsd xslt svg terminal]
   },
   :packages => [
     {'name' => 'CoffeeScript', 'source' => 'https://github.com/Xavura/CoffeeScript-Sublime-Plugin'},
-    {'name' => 'CSV', 'source' => 'https://github.com/ericmartel/Sublime-Text-2-CSV-Plugin'},
-    {'name' => 'ERB Insert and Toggle Commands', 'source' => 'https://github.com/eddorre/SublimeERB'},
-    {'name' => 'FileDiffs', 'source' => 'https://github.com/spape/SublimeFileDiffs'},
+    # {'name' => 'CSV', 'source' => 'https://github.com/ericmartel/Sublime-Text-2-CSV-Plugin'},
+    # {'name' => 'ERB Insert and Toggle Commands', 'source' => 'https://github.com/eddorre/SublimeERB'},
+    # {'name' => 'FileDiffs', 'source' => 'https://github.com/spape/SublimeFileDiffs'},
     {'name' => 'Git', 'source' => 'https://github.com/kemayo/sublime-text-2-git'},
     {'name' => 'GitGutter', 'source' => 'https://github.com/jisaacks/GitGutter'},
-    {'name' => 'Haml', 'source' => 'https://github.com/phuibonhoa/handcrafted-haml-textmate-bundle'},
-    {'name' => 'HTML2HAML', 'source' => 'https://github.com/pavelpachkovskij/sublime-html-to-haml'},
-    {'name' => 'Indent XML', 'source' => 'https://github.com/alek-sys/sublimetext_indentxml'},
-    {'name' => 'JS2Coffee', 'source' => 'https://github.com/nibblebot/sublime-js2coffee'},
-    {'name' => 'LESS', 'source' => 'https://github.com/danro/LESS-sublime'},
+    # {'name' => 'Haml', 'source' => 'https://github.com/phuibonhoa/handcrafted-haml-textmate-bundle'},
+      # {'name' => 'HTML2HAML', 'source' => 'https://github.com/pavelpachkovskij/sublime-html-to-haml'},
+    # {'name' => 'Indent XML', 'source' => 'https://github.com/alek-sys/sublimetext_indentxml'},
+      # {'name' => 'JS2Coffee', 'source' => 'https://github.com/nibblebot/sublime-js2coffee'},
+    # {'name' => 'LESS', 'source' => 'https://github.com/danro/LESS-sublime'},
     {'name' => 'Markdown Preview', 'source' => 'https://github.com/revolunet/sublimetext-markdown-preview'},
-    {'name' => 'Pretty JSON', 'source' => 'https://github.com/dzhibas/SublimePrettyJson'},
-    {'name' => 'Rails Related Files', 'source' => 'https://github.com/luqman/SublimeText2RailsRelatedFiles'},
+    # {'name' => 'Pretty JSON', 'source' => 'https://github.com/dzhibas/SublimePrettyJson'},
+    # {'name' => 'Rails Related Files', 'source' => 'https://github.com/luqman/SublimeText2RailsRelatedFiles'},
     {'name' => 'RSpec', 'source' => 'https://github.com/SublimeText/RSpec'},
     {'name' => 'Ruby 1.9 Hash Converter', 'source' => 'https://github.com/iltempo/sublime-text-2-hash-syntax'},
-    {'name' => 'RubyMotion Autocomplete', 'source' => 'https://github.com/diemer/RubyMotionSublimeCompletions'},
-    {'name' => 'RubyMotionBuilder', 'source' => 'https://github.com/haraken3/SublimeRubyMotionBuilder'},
+    # {'name' => 'RubyMotion Autocomplete', 'source' => 'https://github.com/diemer/RubyMotionSublimeCompletions'},
+    # {'name' => 'RubyMotionBuilder', 'source' => 'https://github.com/haraken3/SublimeRubyMotionBuilder'},
     {'name' => 'RubyTest', 'source' => 'https://github.com/maltize/sublime-text-2-ruby-tests'},
     {'name' => 'Sass', 'source' => 'https://github.com/nathos/sass-textmate-bundle'},
     {'name' => 'SCSS', 'source' => 'https://github.com/kuroir/SCSS.tmbundle'},
-    {'name' => 'Simple Rails Navigator', 'source' => 'https://github.com/noklesta/SublimeRailsNav'},
+    # {'name' => 'Simple Rails Navigator', 'source' => 'https://github.com/noklesta/SublimeRailsNav'},
     {'name' => 'sublime_text_alternative_autocompletion', 'source' => 'https://github.com/alexstaubo/sublime_text_alternative_autocompletion'},
-    {'name' => 'Theme - Soda', 'source' => 'https://github.com/buymeasoda/soda-theme'},
+    # {'name' => 'Theme - Soda', 'source' => 'https://github.com/buymeasoda/soda-theme'},
     {'name' => 'Toggle Symbol to String', 'source' => 'https://github.com/zoomix/SublimeToggleSymbol'},
     {'name' => 'ToggleQuotes', 'source' => 'https://github.com/spadgos/sublime-ToggleQuotes'}
   ]
 }
 
+# memory leak
