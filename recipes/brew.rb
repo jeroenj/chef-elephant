c446e4c8fefcafbf20cfd5c065043fc2f8a7cfd9
Jeroen Jacobs <git@jeroenj.be>
2013-10-09 14:34:48 +0200

    [mariadb] Extracts from brew recipe

--- recipes/brew.rb
+++ recipes/brew.rb
@@ -5,9 +5,3 @@ packages = node[:elephant][:homebrew][:packages]
 packages.each do |pkg|
   package pkg
 end
-
-if packages.include?('mariadb')
-  link "#{ENV['HOME']}/Library/LaunchAgents/homebrew.mxcl.mariadb.plist" do
-    to '/usr/local/opt/mariadb/homebrew.mxcl.mariadb.plist'
-  end
-end
---
+++ recipes/mariadb.rb
@@ -0,0 +1,5 @@
+package 'mariadb'
+
+link "#{ENV['HOME']}/Library/LaunchAgents/homebrew.mxcl.mariadb.plist" do
+  to '/usr/local/opt/mariadb/homebrew.mxcl.mariadb.plist'
+end
