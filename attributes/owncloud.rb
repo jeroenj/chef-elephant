f28fcaadba7ce99751157a215bec7d1c6c46ae8a
Jeroen Jacobs <git@jeroenj.be>
2013-10-23 14:25:48 +0200

    [owncloud] Removes recipe

--- attributes/owncloud.rb
+++
@@ -1,3 +0,0 @@
-default[:elephant][:owncloud][:version] = '1.4.1'
-default[:elephant][:owncloud][:url] = "http://download.owncloud.com/download/ownCloud-#{node[:elephant][:owncloud][:version]}.dmg"
-default[:elephant][:owncloud][:checksum] = '291dd9babe2a16668c5f2890156c2539179f1530'
