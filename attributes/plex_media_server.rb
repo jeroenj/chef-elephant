58a12a3e28afb43bb6a4df1a7dd8b599d53e18e4
Jeroen Jacobs <git@jeroenj.be>
2013-10-31 20:30:37 +0100

    [plex_media_server] Updates to v0.9.8.10.215-020456b
    They also have moved from a dmg package to a zip.

--- attributes/plex_media_server.rb
+++ attributes/plex_media_server.rb
@@ -1,3 +1,3 @@
-default[:elephant][:plex_media_server][:version] = '0.9.8.5.165-c301c50'
-default[:elephant][:plex_media_server][:url] = "http://downloads.plexapp.com/plex-media-server/#{node[:elephant][:plex_media_server][:version]}/PlexMediaServer-#{node[:elephant][:plex_media_server][:version]}-OSX.dmg"
+default[:elephant][:plex_media_server][:version] = '0.9.8.10.215-020456b'
+default[:elephant][:plex_media_server][:url] = "http://downloads.plexapp.com/plex-media-server/#{node[:elephant][:plex_media_server][:version]}/PlexMediaServer-#{node[:elephant][:plex_media_server][:version]}-OSX.zip"
 default[:elephant][:plex_media_server][:checksum] = '5c0d497990f60ee60507c8a08e8332782052c3ae'
--- recipes/plex_media_server.rb
+++ recipes/plex_media_server.rb
@@ -1,6 +1,13 @@
-dmg_package 'Plex Media Server' do
+file = "#{Chef::Config[:file_cache_path]}/plex_media_server_#{node[:elephant][:plex_media_server][:version]}.zip"
+
+remote_file file do
   source node[:elephant][:plex_media_server][:url]
   checksum node[:elephant][:plex_media_server][:checksum]
-  app 'Plex Media Server'
-  volumes_dir 'PlexMediaServer'
+  not_if { ::File.directory?('/Applications/Plex Media Server.app') }
+end
+
+execute 'Install Plex Media Center' do
+  command "unzip #{file}"
+  cwd '/Applications'
+  not_if { ::File.directory?('/Applications/Plex Media Server.app') }
 end
