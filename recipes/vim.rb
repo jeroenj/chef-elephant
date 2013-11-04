8fdab0d728ac14fa4c9985886794827da9ccc434
Jeroen Jacobs <git@jeroenj.be>
2013-11-04 19:43:11 +0100

    [vim] Create config directories

--- recipes/vim.rb
+++ recipes/vim.rb
@@ -2,8 +2,16 @@ cookbook_file "#{ENV['HOME']}/.vimrc" do
   source 'vim/vimrc'
 end
 
+directory "#{ENV['HOME']}/.vim/ftdetect" do
+  recursive true
+end
+
 cookbook_file "#{ENV['HOME']}/.vim/ftdetect/srt.vim" do
   source 'vim/ftdetect/srt.vim'
+end
+
+directory "#{ENV['HOME']}/.vim/syntax" do
+  recursive true
 end
 
 cookbook_file "#{ENV['HOME']}/.vim/syntax/srt.vim" do
