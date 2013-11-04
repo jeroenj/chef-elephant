e8a3afa59b163f87e015ce578bec5ebdb08b99a8
Jeroen Jacobs <git@jeroenj.be>
2013-11-04 09:23:30 +0100

    [java] Updates to 7u45

--- attributes/java.rb
+++ attributes/java.rb
@@ -1,3 +1,3 @@
-default[:elephant][:java][:version] = '7 Update 40'
-default[:elephant][:java][:url] = 'http://javadl.sun.com/webapps/download/AutoDL?BundleId=80806'
-default[:elephant][:java][:checksum] = '4a515e91f5bfcf77f29681c4556b6d616a6f38958461d9f28403a1b89d87927d'
+default[:elephant][:java][:version] = '7 Update 45'
+default[:elephant][:java][:url] = 'http://javadl.sun.com/webapps/download/AutoDL?BundleId=81813'
+default[:elephant][:java][:checksum] = 'b7626e55ceb189a694ae446ebd2bc0a195030417c3ea7c009f1a9adb422b4a10'
