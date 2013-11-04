6bfbd0ce8577de653f6ca0c0c2a2711bc34d6a5c
Jeroen Jacobs <git@jeroenj.be>
2013-11-04 14:04:44 +0100

    [vim] Use ~/.vim/ftdetect/ for file type detection

--- recipes/vim.rb
+++ recipes/vim.rb
@@ -2,6 +2,10 @@ cookbook_file "#{ENV['HOME']}/.vimrc" do
   source 'vim/vimrc'
 end
 
+cookbook_file "#{ENV['HOME']}/.vim/ftdetect/srt.vim" do
+  source 'vim/ftdetect/srt.vim'
+end
+
 cookbook_file "#{ENV['HOME']}/.vim/syntax/srt.vim" do
   source 'vim/syntax/srt.vim'
 end
