4592451775e5c1937b4bbc5d88b514c5954ddea2
Jeroen Jacobs <git@jeroenj.be>
2013-11-11 18:07:55 +0100

    [sabnzbd] Updates to v0.7.16

--- attributes/sabnzbd.rb
+++ attributes/sabnzbd.rb
@@ -1,6 +1,6 @@
-default[:elephant][:sabnzbd][:version] = '0.7.14'
+default[:elephant][:sabnzbd][:version] = '0.7.16'
 default[:elephant][:sabnzbd][:url] = "http://downloads.sourceforge.net/project/sabnzbdplus/sabnzbdplus/#{node[:elephant][:sabnzbd][:version]}/SABnzbd-#{node[:elephant][:sabnzbd][:version]}-osx.dmg"
-default[:elephant][:sabnzbd][:shasum] = '06b2cc099e78b8c70ff606867064f3fd60b22dc7'
+default[:elephant][:sabnzbd][:shasum] = '27c957bb2b68a84c8ba8aa2de4a761b4851873f0'
 
 default[:elephant][:sabnzbd][:preferences][:https] = 0
 default[:elephant][:sabnzbd][:preferences][:https_port] = 9090
