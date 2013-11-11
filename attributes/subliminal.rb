86d1cc63e91a69bc5374c901d198c705a3075531
Jeroen Jacobs <git@jeroenj.be>
2013-11-11 17:14:12 +0100

    [subliminal] Adds support for LaunchAgents

---
+++ attributes/subliminal.rb
@@ -0,0 +1,1 @@
+default[:elephant][:subliminal][:launch_agents] = []
--- recipes/subliminal.rb
+++ recipes/subliminal.rb
@@ -1,3 +1,9 @@
 include_recipe 'elephant::python'
 
 elephant_pip 'subliminal'
+
+node[:elephant][:subliminal][:launch_agents].each do |launch_agent|
+  elephant_plist "#{ENV['HOME']}/Library/LaunchAgents/#{launch_agent[:Label]}.plist" do
+    content launch_agent
+  end
+end
