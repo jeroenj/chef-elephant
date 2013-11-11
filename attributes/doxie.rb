b0fe0c2ee4dc5c85b777374da6ac22d519c7c886
Jeroen Jacobs <git@jeroenj.be>
2013-11-11 18:07:37 +0100

    [doxie] Updates to v2.4.0

--- attributes/doxie.rb
+++ attributes/doxie.rb
@@ -1,2 +1,2 @@
 default[:elephant][:doxie][:url] = 'http://cdn.getdoxie.com/resources/files/Doxie.dmg'
-default[:elephant][:doxie][:checksum] = '34f6dfb260059e85287da5e9fe7ac98a5255cbb188780508ff64673e18587364'
+default[:elephant][:doxie][:checksum] = 'b0995d70616d6849f1629001a5e5bfc123128b65e1b94dd26d42b338ef1df83f'
