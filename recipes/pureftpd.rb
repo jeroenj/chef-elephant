4daf025a88ba5f032b4487c3b95ae577cfdc2fe3
Jeroen Jacobs <git@jeroenj.be>
2013-11-21 13:03:55 +0100

    [pureftpd] Use improved permissions lwrp instead of using the custom code
    The new code will no longer change ownerships of the directory since that is not neccesary. It will only add write access for everyone before writing to the directory. It will also remove that write access again after everything is done.

--- recipes/pureftpd.rb
+++ recipes/pureftpd.rb
@@ -1,30 +1,7 @@
 package 'pure-ftpd'
 
-launch_agents_path = '/Library/LaunchAgents'
-launch_agent_name = 'homebrew.mxcl.pure-ftpd.plist'
-launch_agent_path = File.join launch_agents_path, launch_agent_name
-
-# The following two execute blocks are a hack to allow
-# chef to change files in a root-owned directory.
-ruby_block 'Weaken pureftpd LaunchAgent permissions and ownership' do
-  block do
-    `sudo chmod 777 #{launch_agents_path}`
-    `sudo chown $(whoami):staff #{launch_agent_path}`
-  end
-
-  only_if do
-    src = File.expand_path File.join(__FILE__, '../../files/default/pureftpd', launch_agent_name)
-    !File.exists?(launch_agent_path) || (Chef::Digester.checksum_for_file(launch_agent_path) != Chef::Digester.checksum_for_file(src))
-  end
-  notifies :run, 'ruby_block[Reset pureftpd LaunchAgent permissions and ownership]'
-end
-
-ruby_block 'Reset pureftpd LaunchAgent permissions and ownership' do
-  block do
-    `sudo chmod 755 #{launch_agents_path}`
-    `sudo chown root:wheel #{launch_agent_path}`
-  end
-  action :nothing
+elephant_permissions '/Library/LaunchAgents/homebrew.mxcl.pure-ftpd.plist' do
+  source 'files/default/pureftpd/homebrew.mxcl.pure-ftpd.plist'
 end
 
 cookbook_file '/Library/LaunchAgents/homebrew.mxcl.pure-ftpd.plist' do
