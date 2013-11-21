9f1a8faa6eb7ba678d6eeca7d13c3fecb4115b1b
Jeroen Jacobs <git@jeroenj.be>
2013-11-21 20:15:00 +0100

    Always trigger weaken/reset permissions if no source is given

--- providers/permissions.rb
+++ providers/permissions.rb
@@ -5,8 +5,10 @@ action :override do
     block { `sudo chmod o+w #{destination}` }
 
     only_if do
-      source = ::File.expand_path ::File.join(__FILE__, '../..', new_resource.source)
-      !::File.exists?(new_resource.name) || (Chef::Digester.checksum_for_file(new_resource.name) != Chef::Digester.checksum_for_file(source))
+      !new_resource.source || (
+        source = ::File.expand_path ::File.join(__FILE__, '../..', new_resource.source)
+        !::File.exists?(new_resource.name) || (Chef::Digester.checksum_for_file(new_resource.name) != Chef::Digester.checksum_for_file(source))
+      )
     end
     notifies :run, "ruby_block[Reset #{destination} permissions and ownership]"
   end
