64bedb9af1e66f4708834a9329df7c20e7457e41
Jeroen Jacobs <git@jeroenj.be>
2013-11-21 20:19:33 +0100

    Don't immideately restart launch agents

--- providers/plist.rb
+++ providers/plist.rb
@@ -3,7 +3,7 @@ require 'plist'
 action :write do
   file new_resource.name do
     content new_resource.content.to_plist
-    notifies :restart, "service[#{new_resource.content[:Label]}]", :immediately
+    notifies :restart, "service[#{new_resource.content[:Label]}]"
   end
 
   service new_resource.content[:Label]
