6ba23aaa33b84a49225ff216a311006d2ee78178
Jeroen Jacobs <git@jeroenj.be>
2013-11-21 20:20:17 +0100

    Only restart launch agents if they are in the Users directory

--- providers/plist.rb
+++ providers/plist.rb
@@ -1,10 +1,12 @@
 require 'plist'
 
 action :write do
+  restart = new_resource.name =~ /\/Users\/\w+\/Library\/Launch(Agents|Daemons)\/.*/
+
   file new_resource.name do
     content new_resource.content.to_plist
-    notifies :restart, "service[#{new_resource.content[:Label]}]"
+    notifies(:restart, "service[#{new_resource.content[:Label]}]") if restart
   end
 
-  service new_resource.content[:Label]
+  service(new_resource.content[:Label]) if restart
 end
