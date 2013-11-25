48f411bbb29c9e0b469c75d704ef127fb4fc6d8b
Jeroen Jacobs <git@jeroenj.be>
2013-11-25 13:47:01 +0100

    Adds owner and group attributes to plist resource

--- providers/plist.rb
+++ providers/plist.rb
@@ -5,6 +5,8 @@ action :write do
 
   file new_resource.name do
     content new_resource.content.to_plist
+    owner new_resource.owner
+    group new_resource.group
     notifies(:restart, "service[#{new_resource.content[:Label]}]") if restart
   end
 
--- resources/plist.rb
+++ resources/plist.rb
@@ -2,6 +2,8 @@ actions :write
 
 attribute :name, :kind_of => String, :name_attribute => true
 attribute :content, :kind_of => Hash
+attribute :owner, :kind_of => String, :default => nil
+attribute :group, :kind_of => String, :default => nil
 
 def initialize(name, run_context = nil)
   super
