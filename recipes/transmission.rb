7c85f84a41d86086f5f2a86dacce29c35624bed7
Jeroen Jacobs <git@jeroenj.be>
2013-11-28 18:50:06 +0100

    [tranmsmission] Set remote access password in keychain

--- recipes/transmission.rb
+++ recipes/transmission.rb
@@ -3,6 +3,11 @@ dmg_package 'Transmission' do
   checksum node[:elephant][:transmission][:checksum]
 end
 
+execute 'Set remote access password' do
+  command "security unlock #{ENV['HOME']}/Library/Keychains/login.keychain && security add-generic-password -a Remote -s Transmission:Remote -T /Applications/Transmission -w #{node[:elephant][:transmission][:remote_access_password]} #{ENV['HOME']}/Library/Keychains/login.keychain"
+  not_if { system 'security find-generic-password -s Transmission:Remote' }
+end
+
 if node[:elephant][:transmission][:launch_agent]
   elephant_plist "#{ENV['HOME']}/Library/LaunchAgents/transmission.plist" do
     content node[:elephant][:transmission][:launch_agent]
