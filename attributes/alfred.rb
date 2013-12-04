bebcad595857d6bd71524daead599d71e97b16e2
Jeroen Jacobs <git@jeroenj.be>
2013-12-04 21:01:25 +0100

    [alfred] Updates to v2.1.1

--- attributes/alfred.rb
+++ attributes/alfred.rb
@@ -1,6 +1,6 @@
-default[:elephant][:alfred][:version] = '2.0.3_187'
+default[:elephant][:alfred][:version] = '2.1.1_227'
 default[:elephant][:alfred][:url] = "http://cachefly.alfredapp.com/Alfred_#{node[:elephant][:alfred][:version]}.zip"
-default[:elephant][:alfred][:checksum] = 'd49b747ef4a2b49f401501f813f4cb345b899c691202f998233a03fea06bc0a9'
+default[:elephant][:alfred][:checksum] = 'd19fe7441c6741bf663521e561b842f35707b1e83de21ca195aa033cade66d1b'
 
 default[:elephant][:settings][:alfred] = {
   'domain' => 'com.runningwithcrayons.Alfred-Preferences',
