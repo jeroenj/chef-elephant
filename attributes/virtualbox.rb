91fd588ac819d2e2ca4eb20ea0cd5e32775ea609
Jeroen Jacobs <git@jeroenj.be>
2013-12-04 21:05:32 +0100

    [virtualbox] Updates to v4.3.2

--- attributes/virtualbox.rb
+++ attributes/virtualbox.rb
@@ -1,6 +1,6 @@
-default[:elephant][:virtualbox][:version] = '4.3.2-90405'
+default[:elephant][:virtualbox][:version] = '4.3.4-91027'
 default[:elephant][:virtualbox][:url] = "http://download.virtualbox.org/virtualbox/#{node[:elephant][:virtualbox][:version].split('-').first}/VirtualBox-#{node[:elephant][:virtualbox][:version]}-OSX.dmg"
-default[:elephant][:virtualbox][:checksum] = 'de6d2c433b259162ba68156531954cf4c7fd192e8b47e72ea486988e4fe5939e'
+default[:elephant][:virtualbox][:checksum] = 'b14e427b1a5e11b91900849b22fbf49dd2a178c93520b03424e2d89d2daaf321'
 
 default[:elephant][:settings][:virtualbox] = {
   'domain' => 'org.virtualbox.app.VirtualBox',
