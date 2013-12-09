7cd009f2e078345dfd058e1eff0b52e443f67555
Jeroen Jacobs <git@jeroenj.be>
2013-12-09 19:02:31 +0100

    [subliminal] Create ~/.config directory

--- recipes/subliminal.rb
+++ recipes/subliminal.rb
@@ -3,6 +3,11 @@ include_recipe 'elephant::python'
 elephant_pip 'subliminal'
 elephant_pip 'colorlog'
 
+directory "#{ENV['HOME']}/.config" do
+  owner node[:elephant][:username]
+  group node[:elephant][:group]
+end
+
 node[:elephant][:subliminal][:launch_agents].each do |launch_agent|
   elephant_plist "#{ENV['HOME']}/Library/LaunchAgents/#{launch_agent[:Label]}.plist" do
     content launch_agent
