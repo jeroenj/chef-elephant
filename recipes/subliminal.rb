997ecb1be56335e46248f49e5b2a266cf2beca1c
Jeroen Jacobs <git@jeroenj.be>
2013-12-09 19:02:22 +0100

    [subliminal] Install colorlog

--- recipes/subliminal.rb
+++ recipes/subliminal.rb
@@ -1,6 +1,7 @@
 include_recipe 'elephant::python'
 
 elephant_pip 'subliminal'
+elephant_pip 'colorlog'
 
 node[:elephant][:subliminal][:launch_agents].each do |launch_agent|
   elephant_plist "#{ENV['HOME']}/Library/LaunchAgents/#{launch_agent[:Label]}.plist" do
