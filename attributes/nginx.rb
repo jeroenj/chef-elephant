46b8102ade5690307a779a48c94f4d309d2f1e38
Jeroen Jacobs <git@jeroenj.be>
2013-12-11 20:27:59 +0100

    [nginx] Adds an empty array as default for elephant.nginx.vhosts

--- attributes/nginx.rb
+++ attributes/nginx.rb
@@ -1,11 +1,14 @@
-default[:elephant][:nginx][:launch_agent] = {
-  'Label' => 'nginx',
-  'ProgramArguments' => [
-    '/usr/local/bin/nginx',
-    '-g',
-    'daemon off;'
-  ],
-  'RunAtLoad' => true,
-  'KeepAlive' => true,
-  'WorkingDirectory' => '/usr/local'
+default[:elephant][:nginx] = {
+  :vhosts => [],
+  :launch_agent => {
+    'Label' => 'nginx',
+    'ProgramArguments' => [
+      '/usr/local/bin/nginx',
+      '-g',
+      'daemon off;'
+    ],
+    'RunAtLoad' => true,
+    'KeepAlive' => true,
+    'WorkingDirectory' => '/usr/local'
+  }
 }
