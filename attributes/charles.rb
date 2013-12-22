8686000d03cc65faae06b8f8ec2a0cac6692b603
Jeroen Jacobs <git@jeroenj.be>
2013-12-22 20:39:12 +0100

    [charles] Updates to v3.8.3

--- attributes/charles.rb
+++ attributes/charles.rb
@@ -1,3 +1,3 @@
-default[:elephant][:charles][:version] = '3.8.1'
+default[:elephant][:charles][:version] = '3.8.3'
 default[:elephant][:charles][:url] = "http://www.charlesproxy.com/assets/release/#{node[:elephant][:charles][:version]}/charles-proxy-#{node[:elephant][:charles][:version]}-applejava.dmg"
-default[:elephant][:charles][:checksum] = '5f97cd16b89f8d8b9146f672081a2bc1dc37715ac578fbade41895daee23f2cc'
+default[:elephant][:charles][:checksum] = '6f4fd51d247ea9f992c8c1805c3803522b77a34dedaff1521bd4bc10db63cdb5'
