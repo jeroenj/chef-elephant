0e9606a221bfdab31507644416f08c2156c4b01f
Jeroen Jacobs <git@jeroenj.be>
2014-01-06 22:20:11 +0100

    Enhance installed detection for gem lwrp

--- providers/gem.rb
+++ providers/gem.rb
@@ -7,6 +7,6 @@ action :install do
     command "#{gem_exec} install #{new_resource.name} #{version}"
     user node[:elephant][:username]
     group node[:elephant][:group]
-    not_if { system({'UID' => node[:elephant][:uid]}, "#{gem_exec} list #{new_resource.name} | grep '#{new_resource.name}.*#{new_resource.version}' > /dev/null") }
+    not_if { system({'UID' => node[:elephant][:uid]}, "#{gem_exec} list #{new_resource.name} --installed #{version} > /dev/null") }
   end
 end
