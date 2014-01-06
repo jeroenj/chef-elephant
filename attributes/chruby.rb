cb10c9ad909e9ad85925ff45f452fded1dffd298
Jeroen Jacobs <git@jeroenj.be>
2014-01-06 22:24:54 +0100

    [chruby] Install ruby 2.1.0 and make it default

--- attributes/chruby.rb
+++ attributes/chruby.rb
@@ -1,11 +1,12 @@
 default[:elephant][:chruby] = {
   :path => '/usr/local/var/rubies',
   :rubies => %w[
+    2.1.0
     2.0.0-p353
     1.9.3-p484
     1.8.7-p374
   ],
-  :default => '2.0.0',
+  :default => '2.1.0',
   :gems => [
     { :name => 'bundler', :version => '1.5.1' },
     { :name => 'bundler-audit', :version => '0.3.0' },
