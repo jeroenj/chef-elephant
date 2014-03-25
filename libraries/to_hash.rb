c2b12067494cf26d544740d00850b88b19c9f541
Jeroen Jacobs <git@jeroenj.be>
2014-03-25 13:18:34 +0100

    Adds Chef::Node::ImmutableMash#to_hash patch
    This makes it possible to convert (nested) node attributes to a
    regular hash.

---
+++ libraries/to_hash.rb
@@ -0,0 +1,26 @@
+class Chef
+  class Node
+    class ImmutableMash
+      def to_hash
+        h = {}
+        self.each do |k, v|
+          if v.respond_to?(:to_hash)
+            h[k] = v.to_hash
+          elsif v.respond_to?(:each)
+            h[k] = []
+            v.each do |i|
+              if i.respond_to?(:to_hash)
+                h[k].push(i.to_hash)
+              else
+                h[k].push(i)
+              end
+            end
+          else
+            h[k] = v
+          end
+        end
+        h
+      end
+    end
+  end
+end
