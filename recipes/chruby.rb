03c4386aea87fe7bf6b3d92127f8a4d964c4a99f
Jeroen Jacobs <git@jeroenj.be>
2014-03-28 13:09:15 +0100

    [chruby] Don't install unsupported gems on ruby 1.8.7

--- recipes/chruby.rb
+++ recipes/chruby.rb
@@ -2,11 +2,12 @@ package 'chruby'
 package 'ruby-install'
 
 node[:elephant][:chruby][:rubies].each do |ruby|
+  old_ruby = ruby =~ /.*1\.8\.7.*/
   name = "ruby-#{ruby}"
   path = ::File.join node[:elephant][:chruby][:path], name
 
   cmd = "ruby-install --install-dir #{path} --src-dir /tmp ruby #{ruby}"
-  cmd += " -- --without-tk" if ruby =~ /.*1\.8\.7.*/
+  cmd += " -- --without-tk" if old_ruby
 
   execute "Install ruby #{ruby}" do
     command cmd
@@ -16,15 +17,17 @@ node[:elephant][:chruby][:rubies].each do |ruby|
   end
 
   node[:elephant][:chruby][:gems].each do |ruby_gem|
-    gem_exec = "source /usr/local/opt/chruby/share/chruby/chruby.sh && RUBIES=(/usr/local/var/rubies/ruby-#{ruby}) && chruby #{ruby} && /usr/local/var/rubies/ruby-#{ruby}/bin/gem"
-    version = " --version '#{ruby_gem[:version]}'" if ruby_gem[:version]
-    description_version = " #{ruby_gem[:version]}" if ruby_gem[:version]
+    unless old_ruby && %w[pry-debugger rubocop].include?(ruby_gem[:name])
+      gem_exec = "source /usr/local/opt/chruby/share/chruby/chruby.sh && RUBIES=(/usr/local/var/rubies/ruby-#{ruby}) && chruby #{ruby} && /usr/local/var/rubies/ruby-#{ruby}/bin/gem"
+      version = " --version '#{ruby_gem[:version]}'" if ruby_gem[:version]
+      description_version = " #{ruby_gem[:version]}" if ruby_gem[:version]
 
-    execute "Install #{ruby_gem[:name]}#{description_version} on ruby #{ruby}" do
-      command "#{gem_exec} install #{ruby_gem[:name]}#{version}"
-      user node[:elephant][:username]
-      group node[:elephant][:group]
-      not_if { system({'UID' => node[:elephant][:uid], 'GEM_PATH' => ''}, "#{gem_exec} list #{ruby_gem[:name]} --installed#{version} > /dev/null") }
+      execute "Install #{ruby_gem[:name]}#{description_version} on ruby #{ruby}" do
+        command "#{gem_exec} install #{ruby_gem[:name]}#{version}"
+        user node[:elephant][:username]
+        group node[:elephant][:group]
+        not_if { system({'UID' => node[:elephant][:uid], 'GEM_PATH' => ''}, "#{gem_exec} list #{ruby_gem[:name]} --installed#{version} > /dev/null") }
+      end
     end
   end
 end
