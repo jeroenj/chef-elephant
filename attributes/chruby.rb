b60a0c23cdd6863679fd4f31d426fb3a920f890e
Jeroen Jacobs <git@jeroenj.be>
2014-03-28 12:52:15 +0100

    [chruby] Removes colorful_json from default gems

--- attributes/chruby.rb
+++ attributes/chruby.rb
@@ -10,7 +10,6 @@ default[:elephant][:chruby] = {
     { :name => 'bundler', :version => '~> 1.5.2' },
     { :name => 'bundler-audit', :version => '~> 0.3.0' },
     { :name => 'cheat' },
-    { :name => 'colorful_json' },
     { :name => 'explain_shell' },
     { :name => 'gem-open' },
     { :name => 'mysql2' },
