990520b3590ffc899b2e82f2fe1565f1036c12da
Jeroen Jacobs <git@jeroenj.be>
2014-03-28 12:52:28 +0100

    [ruby] Removes colorful_json from default gems

--- attributes/ruby.rb
+++ attributes/ruby.rb
@@ -2,7 +2,6 @@ default[:elephant][:ruby][:gems] = [
   { :name => 'bundler', :version => '~> 1.5.2' },
   { :name => 'bundler-audit', :version => '~> 0.3.0' },
   { :name => 'cheat' },
-  { :name => 'colorful_json' },
   { :name => 'explain_shell' },
   { :name => 'gem-open' },
   { :name => 'pry' }
