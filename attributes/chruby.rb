eecf1034df3e78602d2966e03238d078510a0de5
Jeroen Jacobs <git@jeroenj.be>
2014-04-23 09:21:01 +0200

    [chruby] Updates bundler-audit to v0.3.1

--- attributes/chruby.rb
+++ attributes/chruby.rb
@@ -8,7 +8,7 @@ default[:elephant][:chruby] = {
   :default => '2.1.1',
   :gems => [
     { :name => 'bundler', :version => '~> 1.6.2' },
-    { :name => 'bundler-audit', :version => '~> 0.3.0' },
+    { :name => 'bundler-audit', :version => '~> 0.3.1' },
     { :name => 'cheat' },
     { :name => 'explain_shell' },
     { :name => 'gem-open' },
