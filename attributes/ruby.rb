b841e13ee5cc87233cc1d1ef6d7f7ab74756a437
Jeroen Jacobs <git@jeroenj.be>
2014-04-23 09:21:12 +0200

    [ruby] Updates bundler-audit to v0.3.1

--- attributes/ruby.rb
+++ attributes/ruby.rb
@@ -1,6 +1,6 @@
 default[:elephant][:ruby][:gems] = [
   { :name => 'bundler', :version => '~> 1.6.2' },
-  { :name => 'bundler-audit', :version => '~> 0.3.0' },
+  { :name => 'bundler-audit', :version => '~> 0.3.1' },
   { :name => 'cheat' },
   { :name => 'explain_shell' },
   { :name => 'gem-open' },
