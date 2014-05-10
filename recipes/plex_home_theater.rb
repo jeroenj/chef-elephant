8e922c0cdd691f70cf92bf4379ae755feb2875b6
Jeroen Jacobs <git@jeroenj.be>
2014-05-10 22:02:05 +0200

    [plex_home_theater] Removes lastfm settings

--- recipes/plex_home_theater.rb
+++ recipes/plex_home_theater.rb
@@ -9,8 +9,6 @@ template "#{ENV['HOME']}/Library/Application Support/Plex Home theater/userdata/
   source 'plex_home_theater/guisettings.xml.erb'
   variables(
     :device_name => node[:elephant][:hostname],
-    :lastfm_encrypted_password => node[:elephant][:plex_home_theater][:preferences][:lastfm_encrypted_password],
-    :lastfm_username => node[:elephant][:plex_home_theater][:preferences][:lastfm_username],
     :myplex_token => node[:elephant][:plex_home_theater][:preferences][:myplex_token],
     :myplex_username => node[:elephant][:plex_home_theater][:preferences][:myplex_username],
     :version => node[:elephant][:plex_home_theater][:version]
