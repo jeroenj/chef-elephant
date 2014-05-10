39a7b268eed18a0ae9a9575e1de0c75b0d793a38
Jeroen Jacobs <git@jeroenj.be>
2014-05-10 22:17:53 +0200

    [alfred] Use homebrew cask for installation

--- attributes/alfred.rb
+++ attributes/alfred.rb
@@ -1,7 +1,3 @@
-default[:elephant][:alfred][:version] = '2.1.1_227'
-default[:elephant][:alfred][:url] = "http://cachefly.alfredapp.com/Alfred_#{node[:elephant][:alfred][:version]}.zip"
-default[:elephant][:alfred][:checksum] = 'd19fe7441c6741bf663521e561b842f35707b1e83de21ca195aa033cade66d1b'
-
 default[:elephant][:alfred][:license] = nil
 
 default[:elephant][:settings][:alfred] = {
--- recipes/alfred.rb
+++ recipes/alfred.rb
@@ -1,16 +1,6 @@
 # include elephant::settings at the end of the run list to include settings
 
-remote_file "#{Chef::Config[:file_cache_path]}/alfred_#{node[:elephant][:alfred][:version]}.zip" do
-  checksum node[:elephant][:alfred][:checksum]
-  source node[:elephant][:alfred][:url]
-  not_if { ::File.directory?('/Applications/Alfred 2.app') }
-end
-
-execute 'Install Alfred' do
-  command "unzip #{Chef::Config[:file_cache_path]}/alfred_#{node[:elephant][:alfred][:version]}.zip"
-  cwd '/Applications'
-  not_if { ::File.directory?('/Applications/Alfred 2.app') }
-end
+homebrew_cask 'alfred'
 
 if node[:elephant][:alfred][:license]
   directory "#{ENV['HOME']}/Library/Application Support/Alfred 2" do
