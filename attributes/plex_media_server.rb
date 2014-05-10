ecd5e90f31f0c0265bdf1d130bf4c5466d61c6dd
Jeroen Jacobs <git@jeroenj.be>
2014-05-10 21:58:41 +0200

    [plex_media_server] Use homebrew cask for installation

--- attributes/plex_media_server.rb
+++ attributes/plex_media_server.rb
@@ -1,7 +1,3 @@
-default[:elephant][:plex_media_server][:version] = '0.9.8.10.215-020456b'
-default[:elephant][:plex_media_server][:url] = "http://downloads.plexapp.com/plex-media-server/#{node[:elephant][:plex_media_server][:version]}/PlexMediaServer-#{node[:elephant][:plex_media_server][:version]}-OSX.zip"
-default[:elephant][:plex_media_server][:checksum] = '5c0d497990f60ee60507c8a08e8332782052c3ae'
-
 default[:elephant][:settings][:plex_media_server] = {
   'domain' => 'com.plexapp.plexmediaserver',
   'AcceptedEULA' => true,
--- recipes/plex_media_server.rb
+++ recipes/plex_media_server.rb
@@ -1,16 +1,4 @@
-file = "#{Chef::Config[:file_cache_path]}/plex_media_server_#{node[:elephant][:plex_media_server][:version]}.zip"
-
-remote_file file do
-  source node[:elephant][:plex_media_server][:url]
-  checksum node[:elephant][:plex_media_server][:checksum]
-  not_if { ::File.directory?('/Applications/Plex Media Server.app') }
-end
-
-execute 'Install Plex Media Server' do
-  command "unzip #{file}"
-  cwd '/Applications'
-  not_if { ::File.directory?('/Applications/Plex Media Server.app') }
-end
+homebrew_cask 'plex-media-server'
 
 elephant_plist "#{ENV['HOME']}/Library/LaunchAgents/plex-media-server.plist" do
   content node[:elephant][:plex_media_server][:launch_agent]
