60cdfee6cc5c8a72add11475ddfd741b86e5381c
Jeroen Jacobs <git@jeroenj.be>
2014-05-16 13:53:59 +0200

    [prezto] Adds recipe

---
+++ attributes/prezto.rb
@@ -0,0 +1,1 @@
+default[:elephant][:prezto][:url] = 'git@github.com:jeroenj/prezto.git'
---
+++ recipes/prezto.rb
@@ -0,0 +1,16 @@
+include_recipe 'elephant::zsh'
+
+git "#{ENV['HOME']}/.zprezto" do
+  repository node[:elephant][:prezto][:url]
+  action :sync
+  user node[:elephant][:username]
+  group node[:elephant][:group]
+end
+
+%w[zlogin zlogout zpreztorc zprofile zshenv zshrc].each do |file|
+  link "#{ENV['HOME']}/.#{file}" do
+    to "#{ENV['HOME']}/.zprezto/runcoms/#{file}"
+    user node[:elephant][:username]
+    group node[:elephant][:group]
+  end
+end
