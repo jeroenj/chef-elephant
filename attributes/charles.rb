0782b2122dd7b5a7073187c08318f30bafc65aa6
Jeroen Jacobs <git@jeroenj.be>
2014-05-19 22:15:15 +0200

    [charles] Use homebrew cask for installation

--- attributes/charles.rb
+++
@@ -1,3 +0,0 @@
-default[:elephant][:charles][:version] = '3.8.3'
-default[:elephant][:charles][:url] = "http://www.charlesproxy.com/assets/release/#{node[:elephant][:charles][:version]}/charles-proxy-#{node[:elephant][:charles][:version]}-applejava.dmg"
-default[:elephant][:charles][:checksum] = '6f4fd51d247ea9f992c8c1805c3803522b77a34dedaff1521bd4bc10db63cdb5'
