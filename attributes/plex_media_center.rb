baddae026d77d3549b2b6bff099b3b8040ab33ce
Jeroen Jacobs <git@jeroenj.be>
2014-05-20 13:01:09 +0200

    [plex_media_center] Removes recipe

--- attributes/plex_media_center.rb
+++
@@ -1,7 +0,0 @@
-default[:elephant][:plex_media_center][:version] = '0.9.5.4'
-default[:elephant][:plex_media_center][:tag] = '973998f'
-default[:elephant][:plex_media_center][:url] = "http://plex.r.worldssl.net/plex-laika/#{node[:elephant][:plex_media_center][:version]}/Plex-#{node[:elephant][:plex_media_center][:version]}-#{node[:elephant][:plex_media_center][:tag]}.zip"
-default[:elephant][:plex_media_center][:checksum] = '6d544de6c56861eab05a5621ecee1777cefe49fc'
-
-default[:elephant][:plex_media_center][:preferences][:myplex] = {}
-default[:elephant][:plex_media_center][:preferences][:lastfm] = {}
