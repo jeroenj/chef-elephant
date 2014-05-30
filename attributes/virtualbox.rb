8caf82596fad9948f655ae3b851708ebd87053f9
Jeroen Jacobs <git@jeroenj.be>
2014-05-30 23:14:11 +0200

    [virtualbox] Use homebrew cask for installation

--- attributes/virtualbox.rb
+++ attributes/virtualbox.rb
@@ -1,7 +1,3 @@
-default[:elephant][:virtualbox][:version] = '4.3.6-91406'
-default[:elephant][:virtualbox][:url] = "http://download.virtualbox.org/virtualbox/#{node[:elephant][:virtualbox][:version].split('-').first}/VirtualBox-#{node[:elephant][:virtualbox][:version]}-OSX.dmg"
-default[:elephant][:virtualbox][:checksum] = '60678b7cc7dc741b800cfc99c7ff73a185c548af877071eb82422f354e76bca2'
-
 default[:elephant][:settings][:virtualbox] = {
   'domain' => 'org.virtualbox.app.VirtualBox',
   'NSNavLastRootDirectory' => "#{ENV['HOME']}/VMs"
--- recipes/virtualbox.rb
+++ recipes/virtualbox.rb
@@ -1,8 +1,3 @@
 # include elephant::settings at the end of the run list to include settings
 
-dmg_package 'VirtualBox' do
-  source node[:elephant][:virtualbox][:url]
-  checksum node[:elephant][:virtualbox][:checksum]
-  type 'pkg'
-  package_id 'org.virtualbox.pkg.virtualbox'
-end
+homebrew_cask 'virtualbox'
