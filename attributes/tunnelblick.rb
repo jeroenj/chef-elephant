214955c33c56ed1cdff2c0c39cdcd55f23033549
Jeroen Jacobs <git@jeroenj.be>
2014-05-30 23:12:35 +0200

    [tunnelblick] Use homebrew cask for installation

--- attributes/tunnelblick.rb
+++
@@ -1,3 +0,0 @@
-default[:elephant][:tunnelblick][:version] = '3.3'
-default[:elephant][:tunnelblick][:url] = "http://sourceforge.net/projects/tunnelblick/files/All%20files/Tunnelblick_#{node[:elephant][:tunnelblick][:version]}.dmg/download"
-default[:elephant][:tunnelblick][:checksum] = 'c950430cc9a21f6109ff44e8509636d8b4c82cf9'
--- recipes/tunnelblick.rb
+++ recipes/tunnelblick.rb
@@ -1,5 +1,1 @@
-dmg_package 'Tunnelblick' do
-  dmg_name "Tunnelblick_#{node[:elephant][:tunnelblick][:version]}"
-  source node[:elephant][:tunnelblick][:url]
-  checksum node[:elephant][:tunnelblick][:checksum]
-end
+homebrew_cask 'tunnelblick'
