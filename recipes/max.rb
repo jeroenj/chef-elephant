f1cc782f1f575b4ecc631f60b57b7919199fc5d2
Jeroen Jacobs <git@jeroenj.be>
2014-05-30 22:43:00 +0200

    [max] Use homebrew cask for installation

--- recipes/max.rb
+++ recipes/max.rb
@@ -1,13 +1,3 @@
 # include elephant::settings at the end of the run list to include settings
 
-remote_file "#{Chef::Config[:file_cache_path]}/max_#{node[:elephant][:max][:version]}.zip" do
-  checksum node[:elephant][:max][:checksum]
-  source node[:elephant][:max][:url]
-  not_if { ::File.directory?("/Applications/Max-#{node[:elephant][:max][:version]}/Max.app") }
-end
-
-execute 'Install Max' do
-  command "tar -jxf #{Chef::Config[:file_cache_path]}/max_#{node[:elephant][:max][:version]}.zip"
-  cwd '/Applications'
-  not_if { ::File.directory?("/Applications/Max-#{node[:elephant][:max][:version]}/Max.app") }
-end
+homebrew_cask 'max'
