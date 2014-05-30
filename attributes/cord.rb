970392cbb5e4109b86f1e348ec2f57b8df41bae8
Jeroen Jacobs <git@jeroenj.be>
2014-05-30 21:39:35 +0200

    [cord] Use homebrew cask for installation

--- attributes/cord.rb
+++ attributes/cord.rb
@@ -1,7 +1,3 @@
-default[:elephant][:cord][:version] = '0.5.7'
-default[:elephant][:cord][:url] = "http://sourceforge.net/projects/cord/files/cord/#{node[:elephant][:cord][:version]}/CoRD_#{node[:elephant][:cord][:version]}.zip/download"
-default[:elephant][:cord][:checksum] = '8f505b12b94167100b3b8b44ed3cee32ffcc94b73dc44fe0ecc896151f114100'
-
 default[:elephant][:settings][:cord] = {
   'domain' => 'net.sf.cord',
   'CRDBaseConnectionScreenSize' => '1280x800',
--- recipes/cord.rb
+++ recipes/cord.rb
@@ -1,11 +1,1 @@
-remote_file "#{Chef::Config[:file_cache_path]}/cord_#{node[:elephant][:cord][:version]}.zip" do
-  checksum node[:elephant][:cord][:checksum]
-  source node[:elephant][:cord][:url]
-  not_if { ::File.directory?('/Applications/Cord.app') }
-end
-
-execute 'Install Cord' do
-  command "unzip #{Chef::Config[:file_cache_path]}/cord_#{node[:elephant][:cord][:version]}.zip"
-  cwd '/Applications'
-  not_if { ::File.directory?('/Applications/Cord.app') }
-end
+homebrew_cask 'cord'
