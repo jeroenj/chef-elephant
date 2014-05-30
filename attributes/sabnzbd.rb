4179cfaebc09dac421e9362e7c0072e42b8935ae
Jeroen Jacobs <git@jeroenj.be>
2014-05-30 23:49:08 +0200

    [sabnzbd] Use homebrew cask for installation

--- attributes/sabnzbd.rb
+++ attributes/sabnzbd.rb
@@ -1,7 +1,3 @@
-default[:elephant][:sabnzbd][:version] = '0.7.16'
-default[:elephant][:sabnzbd][:url] = "http://downloads.sourceforge.net/project/sabnzbdplus/sabnzbdplus/#{node[:elephant][:sabnzbd][:version]}/SABnzbd-#{node[:elephant][:sabnzbd][:version]}-osx.dmg"
-default[:elephant][:sabnzbd][:shasum] = '27c957bb2b68a84c8ba8aa2de4a761b4851873f0'
-
 default[:elephant][:sabnzbd][:preferences][:https] = 0
 default[:elephant][:sabnzbd][:preferences][:https_port] = 9090
 default[:elephant][:sabnzbd][:preferences][:https_key] = '""'
@@ -12,7 +8,7 @@ default[:elephant][:sabnzbd][:preferences][:servers] = []
 
 default[:elephant][:sabnzbd][:launch_agent] = {
   'Label' => 'sabnzbd',
-  'Program' => '/Applications/SABnzbd.app/Contents/MacOS/SABnzbd',
+  'Program' => "#{ENV['HOME']}/Applications/SABnzbd.app/Contents/MacOS/SABnzbd",
   'RunAtLoad' => true,
   'KeepAlive' => {
     'SuccessfulExit' => false
--- recipes/sabnzbd.rb
+++ recipes/sabnzbd.rb
@@ -1,44 +1,4 @@
-# dmg_package 'SABnzbd' do
-#   source node[:elephant][:sabnzbd][:url]
-#   checksum node[:elephant][:sabnzbd][:checksum]
-#   volumes_dir "SABnzbd-#{node[:elephant][:sabnzbd][:version]}/OS X 10.8 (Mountain Lion)"
-# end
-
-# Since the app is inside a folder in the dmg we can not use the dmg_package lwrp.
-# Below are all the commands needed to install the app. It does exactly the same as
-# dmg_package, but uses the correct folders.
-# ignore_failure was not a noption for dmg_package either, since it fails from one
-# of its sub lwrp's: the shellout to detach the image failed.
-
-volumes_dir = "/Volumes/SABnzbd-#{node[:elephant][:sabnzbd][:version]}"
-app_path = '/Applications/SABnzbd.app'
-
-dmg_file = "#{Chef::Config[:file_cache_path]}/SABnzbd-#{node[:elephant][:sabnzbd][:version]}.dmg"
-
-remote_file dmg_file do
-  source node[:elephant][:sabnzbd][:url]
-  checksum node[:elephant][:sabnzbd][:checksum]
-  not_if { ::File.directory? app_path }
-end
-
-execute "hdiutil attach '#{dmg_file}'" do
-  not_if { ::File.directory? app_path }
-end
-
-execute "cp -R '#{volumes_dir}/OS X 10.8 (Mountain Lion)/SABnzbd.app' '/Applications'" do
-  not_if { ::File.directory? app_path }
-end
-
-file "#{app_path}/Contents/MacOS/SABnzbd.app" do
-  mode 0755
-  ignore_failure true
-end
-
-execute "hdiutil detach '#{volumes_dir}'" do
-  only_if { ::File.directory? volumes_dir }
-end
-
-# dmg_package replacement ends here
+homebrew_cask 'sabnzbd'
 
 if node[:elephant][:sabnzbd][:preferences]
   settings_path = "#{ENV['HOME']}/Library/Application Support/SABnzbd"
