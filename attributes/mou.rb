4b75a81f52b40b91f1aa3e6efe0a2dfcaa055c20
Jeroen Jacobs <git@jeroenj.be>
2014-05-30 22:43:50 +0200

    [mou] Use homebrew cask for installation

--- attributes/mou.rb
+++ attributes/mou.rb
@@ -1,5 +1,3 @@
-default[:elephant][:mou][:url] = 'http://mouapp.com/download/Mou.zip'
-
 default[:elephant][:settings][:mou] = {
   'domain' => 'com.mouapp.Mou',
   'openMouStandPanelKeyString' => true,
