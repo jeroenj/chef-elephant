f7eea622ea5ff7223e24703c154125df55d0e1a8
Jeroen Jacobs <git@jeroenj.be>
2014-05-30 23:07:03 +0200

    [teamviewer] Use homebrew cask for installation

--- attributes/teamviewer.rb
+++
@@ -1,2 +0,0 @@
-default[:elephant][:teamviewer][:url] = 'http://downloadeu1.teamviewer.com/download/TeamViewer.dmg'
-default[:elephant][:teamviewer][:checksum] = '2dd607411978b9ef774e9d299b95d0f2cde3ecbd22293e3dd4f564f97c671cc6'
--- recipes/teamviewer.rb
+++ recipes/teamviewer.rb
@@ -1,8 +1,1 @@
-dmg_package 'TeamViewer' do
-  source node[:elephant][:teamviewer][:url]
-  checksum node[:elephant][:teamviewer][:checksum]
-  app 'Install TeamViewer'
-  volumes_dir 'TeamViewer'
-  type 'pkg'
-  package_id 'com.teamviewer.teamviewer9'
-end
+homebrew_cask 'teamviewer'
