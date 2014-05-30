1086ee4a64b27fe7da03fca8f893ba66f526d48e
Jeroen Jacobs <git@jeroenj.be>
2014-05-30 23:19:57 +0200

    [vmware_fusion] Removes recipe

--- attributes/vmware_fusion.rb
+++
@@ -1,3 +0,0 @@
-default[:elephant][:vmware_fusion][:version] = '6.0.2-1398658'
-default[:elephant][:vmware_fusion][:url] = "https://download3.vmware.com/software/fusion/file/VMware-Fusion-#{node[:elephant][:vmware_fusion][:version]}-light.dmg"
-default[:elephant][:vmware_fusion][:checksum] = 'e20dbdff865a45eb8e3f08eb69f3722b1f0d0478d6d7eb4bc9ed6077d78d9f9c'
