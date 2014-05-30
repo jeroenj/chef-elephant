007c0745f7621fa84d005d7240ad792d9d8d8123
Jeroen Jacobs <git@jeroenj.be>
2014-05-30 21:40:32 +0200

    [cyberduck] Use homebrew cask for installation

--- attributes/cyberduck.rb
+++ attributes/cyberduck.rb
@@ -1,7 +1,3 @@
-default[:elephant][:cyberduck][:version] = '4.3.1'
-default[:elephant][:cyberduck][:url] = "http://cyberduck.ch/Cyberduck-#{node[:elephant][:cyberduck][:version]}.zip"
-default[:elephant][:cyberduck][:checksum] = '5a162b2dfc3c6a61b6ef91c6a4ac682161ca58b2'
-
 default[:elephant][:cyberduck][:bookmarks] = []
 
 default[:elephant][:settings][:cyberduck] = {
--- recipes/cyberduck.rb
+++ recipes/cyberduck.rb
@@ -1,16 +1,6 @@
 # include elephant::settings at the end of the run list to include settings
 
-remote_file "#{Chef::Config[:file_cache_path]}/cyberduck_#{node[:elephant][:cyberduck][:version]}.zip" do
-  checksum node[:elephant][:cyberduck][:checksum]
-  source node[:elephant][:cyberduck][:url]
-  not_if { ::File.directory?('/Applications/Cyberduck.app') }
-end
-
-execute 'Install Cyberduck' do
-  command "unzip #{Chef::Config[:file_cache_path]}/cyberduck_#{node[:elephant][:cyberduck][:version]}.zip"
-  cwd '/Applications'
-  not_if { ::File.directory?('/Applications/Cyberduck.app') }
-end
+homebrew_cask 'cyberduck'
 
 elephant_recursive_directory "#{ENV['HOME']}/Library/Application Support/Cyberduck/Bookmarks" do
   owner node[:elephant][:username]
