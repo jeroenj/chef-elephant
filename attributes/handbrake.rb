a2277de32fdd24eb559c8ee4b0537244698f9838
Jeroen Jacobs <git@jeroenj.be>
2014-05-30 22:26:56 +0200

    [homebrew] Use homebrew cask for installation

--- attributes/handbrake.rb
+++
@@ -1,3 +0,0 @@
-default[:elephant][:handbrake][:version] = '0.9.9'
-default[:elephant][:handbrake][:url] = "http://sourceforge.net/projects/handbrake/files/#{node[:elephant][:handbrake][:version]}/HandBrake-#{node[:elephant][:handbrake][:version]}-MacOSX.6_GUI_x86_64.dmg/download"
-default[:elephant][:handbrake][:checksum] = '9b6f0259f3378b0cc136378d7859162aa95c0eb7'
--- recipes/handbrake.rb
+++ recipes/handbrake.rb
@@ -1,5 +1,1 @@
-dmg_package 'Handbrake' do
-  source node[:elephant][:handbrake][:url]
-  checksum node[:elephant][:handbrake][:checksum]
-  volumes_dir "HandBrake-#{node[:elephant][:handbrake][:version]}-MacOSX.6_GUI_x86_64"
-end
+homebrew_cask 'handbrake'
