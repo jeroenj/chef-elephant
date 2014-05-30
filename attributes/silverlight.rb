b712dfcd8e7818482303b6d9f73847a6317b1e9a
Jeroen Jacobs <git@jeroenj.be>
2014-05-30 22:53:41 +0200

    [silverlight] Use homebrew cask for installation

--- attributes/silverlight.rb
+++
@@ -1,2 +0,0 @@
-default[:elephant][:silverlight][:url] = 'http://silverlight.dlservice.microsoft.com/download/6/B/8/6B885B0B-73D4-4BCE-BFFF-154B4A402D22/20913.00/Silverlight.dmg'
-default[:elephant][:silverlight][:checksum] = 'f3e9e1bf30adbeb891ce44595541acba7d35b23c03f96a2b7df91b15325bf4cf'
--- recipes/silverlight.rb
+++ recipes/silverlight.rb
@@ -1,6 +1,1 @@
-dmg_package 'Silverlight' do
-  source node[:elephant][:silverlight][:url]
-  checksum node[:elephant][:silverlight][:checksum]
-  type 'pkg'
-  package_id 'com.microsoft.SilverlightInstaller'
-end
+homebrew_cask 'silverlight'
