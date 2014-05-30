8afc51638f77df16314a76b9d04d1db1e19e8ccd
Jeroen Jacobs <git@jeroenj.be>
2014-05-30 22:29:13 +0200

    [hipchat] Use homebrew cask for installation

--- attributes/hipchat.rb
+++ attributes/hipchat.rb
@@ -1,7 +1,3 @@
-default[:elephant][:hipchat][:version] = '2.0'
-default[:elephant][:hipchat][:url] = "http://downloads.hipchat.com.s3.amazonaws.com/osx/HipChat-#{node[:elephant][:hipchat][:version]}.zip"
-default[:elephant][:hipchat][:checksum] = '1552c87662d6d31161fba447c28e9f9c3ace1377'
-
 default[:elephant][:settings][:hipchat] = {
   'domain' => 'com.hipchat.HipChat',
   'notifyForPrivate' => true,
--- recipes/hipchat.rb
+++ recipes/hipchat.rb
@@ -1,13 +1,3 @@
 # include elephant::settings at the end of the run list to include settings
 
-remote_file "#{Chef::Config[:file_cache_path]}/hipchat_#{node[:elephant][:hipchat][:version]}.zip" do
-  checksum node[:elephant][:hipchat][:checksum]
-  source node[:elephant][:hipchat][:url]
-  not_if { ::File.directory?('/Applications/HipChat.app') }
-end
-
-execute 'Install Hipchat' do
-  command "unzip #{Chef::Config[:file_cache_path]}/hipchat_#{node[:elephant][:hipchat][:version]}.zip"
-  cwd '/Applications'
-  not_if { ::File.directory?('/Applications/HipChat.app') }
-end
+homebrew_cask 'hipchat'
