2610dfc2096c439ae1e2e466c6586fa3c5c5b3f4
Jeroen Jacobs <git@jeroenj.be>
2014-05-30 23:29:26 +0200

    [xquartz] Use homebrew cask for installation

--- attributes/xquartz.rb
+++
@@ -1,3 +0,0 @@
-default[:xquartz][:version] = '2.7.5'
-default[:xquartz][:url] = "http://xquartz-dl.macosforge.org/SL/XQuartz-#{node['xquartz']['version']}.dmg"
-default[:xquartz][:checksum] = '4382ff78cef5630fb6b8cc982da2e5a577d8cc5dddd35a493b50bad2fcf5e34a'
--- recipes/xquartz.rb
+++ recipes/xquartz.rb
@@ -1,7 +1,1 @@
-dmg_package 'XQuartz' do
-  source node[:xquartz][:url]
-  checksum node[:xquartz][:checksum]
-  volumes_dir "XQuartz-#{node['xquartz']['version']}"
-  package_id 'org.macosforge.xquartz.pkg'
-  type 'pkg'
-end
+homebrew_cask 'xquartz'
