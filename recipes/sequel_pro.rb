79892ec319bb804bc875569c7e4016f7023a9d30
Jeroen Jacobs <git@jeroenj.be>
2014-05-30 22:50:43 +0200

    [sequel_pro] Use homebrew cask for installation

--- recipes/sequel_pro.rb
+++ recipes/sequel_pro.rb
@@ -1,7 +1,3 @@
 # include elephant::settings at the end of the run list to include settings
 
-dmg_package 'Sequel Pro' do
-  volumes_dir "Sequel Pro #{node[:elephant][:sequel_pro][:version]}"
-  source node[:elephant][:sequel_pro][:url]
-  checksum node[:elephant][:sequel_pro][:checksum]
-end
+homebrew_cask 'sequel-pro'
