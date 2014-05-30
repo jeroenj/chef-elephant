6c9ab03f7ef974b45421e41026aa5e1817d6374c
Jeroen Jacobs <git@jeroenj.be>
2014-05-30 22:18:11 +0200

    [gitx] Use homebrew cask for installation

--- recipes/gitx.rb
+++ recipes/gitx.rb
@@ -1,19 +1,3 @@
 # include elephant::settings at the end of the run list to include settings
 
-remote_file "#{Chef::Config[:file_cache_path]}/GitX-L_v0.8.4.zip" do
-  checksum 'c5f4088497abf5a219bb7bde4fae643fec61647be25bf836fd679567dcabd7df'
-  source 'https://github.com/downloads/laullon/gitx/GitX-L_v0.8.4.zip'
-  not_if { ::File.directory?('/Applications/GitX.app') }
-end
-
-execute 'Install GitX' do
-  command "unzip #{Chef::Config[:file_cache_path]}/GitX-L_v0.8.4.zip"
-  cwd '/Applications'
-  not_if { ::File.directory?('/Applications/GitX.app') }
-end
-
-link '/usr/local/bin/gitx' do
-  to '/Applications/Gitx.app/Contents/resources/gitx'
-  owner node[:elephant][:username]
-  group node[:elephant][:group]
-end
+homebrew_cask 'gitx-l'
