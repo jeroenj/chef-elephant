b68bbdab3ce796abbbba0fb1889b501f72106725
Jeroen Jacobs <git@jeroenj.be>
2014-06-16 12:10:44 +0200

    [ruby] Don't install gem-open

--- attributes/ruby.rb
+++ attributes/ruby.rb
@@ -3,6 +3,5 @@ default[:elephant][:ruby][:gems] = [
   { :name => 'bundler-audit', :version => '~> 0.3.1' },
   { :name => 'cheat' },
   { :name => 'explain_shell' },
-  { :name => 'gem-open' },
   { :name => 'pry', :version => '~> 0.10.0' }
 ]
