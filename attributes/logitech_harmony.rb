b710cc36bcdb3d228edc5704af9af7ca6b70aecf
Jeroen Jacobs <git@jeroenj.be>
2014-06-30 23:09:29 +0200

    [logitech_harmony] Updates to MyHarmony software. Use homebrew cask for installation.

--- attributes/logitech_harmony.rb
+++
@@ -1,2 +0,0 @@
-default[:elephant][:logitech_harmony][:url] = 'https://files.myharmony.com/Assets/Plugins/MacOSX10.8/LogitechHarmonySoftware.dmg'
-default[:elephant][:logitech_harmony][:checksum] = 'c55a9d6cee8bd10e8a2308ac43ff626458bf6299764059026f08a2e482864840'
--- recipes/logitech_harmony.rb
+++ recipes/logitech_harmony.rb
@@ -1,9 +1,3 @@
 include_recipe 'elephant::silverlight'
 
-dmg_package 'LogitechHarmonySoftware' do
-  source node[:elephant][:logitech_harmony][:url]
-  checksum node[:elephant][:logitech_harmony][:checksum]
-  volumes_dir 'LogitechHarmonySoftware.pkg'
-  type 'pkg'
-  package_id 'com.logitech.harmony.harmonyBrowserPlugin.LogitechHarmony.pkg'
-end
+homebrew_cask 'logitech-myharmony'
