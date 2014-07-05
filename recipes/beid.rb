6685cad3022a3db25f5c78433005ceb44df3393f
Jeroen Jacobs <git@jeroenj.be>
2014-07-05 21:25:15 +0200

    [beid] Use homebrew cask for installation

--- recipes/beid.rb
+++ recipes/beid.rb
@@ -1,8 +1,1 @@
-dmg_package 'beid' do
-  source node[:elephant][:beid][:url]
-  checksum node[:elephant][:beid][:checksum]
-  app "beid-#{node[:elephant][:beid][:version].split('-').last}"
-  volumes_dir "beid-#{node[:elephant][:beid][:version].split('-').first}"
-  type 'pkg'
-  package_id 'be.eid.middleware'
-end
+homebrew_cask 'eid-mw'
