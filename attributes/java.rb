d4df19780fe6d625af0e39d70c90d7f1421231fd
Jeroen Jacobs <git@jeroenj.be>
2014-07-15 10:06:13 +0200

    [java] Use homebrew cask for installation

--- attributes/java.rb
+++
@@ -1,3 +0,0 @@
-default[:elephant][:java][:version] = '7 Update 51'
-default[:elephant][:java][:url] = 'http://javadl.sun.com/webapps/download/AutoDL?BundleId=83377'
-default[:elephant][:java][:checksum] = '8541090bf8bd7b284f07d4b1f74b5352b8addf5e0274eeb82cacdc4b2e2b66d2'
--- recipes/java.rb
+++ recipes/java.rb
@@ -1,6 +1,1 @@
-dmg_package "Java #{node[:elephant][:java][:version]}" do
-  source node[:elephant][:java][:url]
-  checksum node[:elephant][:java][:checksum]
-  type 'pkg'
-  package_id 'com.oracle.jre'
-end
+homebrew_cask 'java'
