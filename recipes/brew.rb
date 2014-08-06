3af86249c16ffcbc4eb85e41913590e63df0a239
Jeroen Jacobs <git@jeroenj.be>
2014-08-06 16:02:23 +0200

    [brew] Use install_formulas recipe to install packages

--- recipes/brew.rb
+++ recipes/brew.rb
@@ -1,15 +1,11 @@
 include_recipe 'homebrew'
+include_recipe 'homebrew::install_formulas'
+
 homebrew_tap 'homebrew/dupes'
 homebrew_tap 'caskroom/cask'
 homebrew_tap 'caskroom/versions'
 
 package 'brew-cask'
-
-packages = node[:elephant][:homebrew][:packages]
-
-packages.each do |pkg|
-  package pkg
-end
 
 elephant_recursive_directory '/usr/local/var/log' do
   owner node[:elephant][:username]
