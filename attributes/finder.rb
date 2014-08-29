7846c4a6e9dc08b130ac9c2365313074c2a843ac
Jeroen Jacobs <git@jeroenj.be>
2014-08-29 16:24:02 +0200

    [finder] Disable extension change warning

--- attributes/finder.rb
+++ attributes/finder.rb
@@ -2,6 +2,7 @@ default[:elephant][:settings][:finder] = {
   'domain' => 'com.apple.finder',
   '_FXShowPosixPathInTitle' => true,
   'FXDefaultSearchScope' => 'SCcf',
+  'FXEnableExtensionChangeWarning' => false,
   'NewWindowTarget' => 'PfHm',
   'ShowStatusBar' => true,
   'WarnOnEmptyTrash' => false
