74faec9f982d4dc6daf9a76460e9ac1d4187625b
Jeroen Jacobs <git@jeroenj.be>
2014-10-22 16:57:40 +0200

    [logrotate] Fixes default filename
    newsyslog does not support **. Since we know the exact directory
    structure we can just use */*.

--- attributes/logrotate.rb
+++ attributes/logrotate.rb
@@ -1,6 +1,6 @@
 default[:elephant][:logrotate][:files] = [
   {
-    :filename => "#{ENV['HOME']}/Development/**/log/*.log",
+    :filename => "#{ENV['HOME']}/Development/*/*/log/*.log",
     :owner_group => "#{node[:elephant][:username]}:staff",
     :mode => 644,
     :count => 0,
