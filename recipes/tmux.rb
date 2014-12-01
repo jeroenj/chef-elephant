a7e2710ee3d6d4646f1bc300b6f18398df461c2b
Jeroen Jacobs <git@jeroenj.be>
2014-12-01 09:40:01 +0100

    [tmux] Adds recipe which installs tmux and sets ~/.tmux.conf

---
+++ recipes/tmux.rb
@@ -0,0 +1,8 @@
+package 'reattach-to-user-namespace'
+package 'tmux'
+
+cookbook_file "#{ENV['HOME']}/.tmux.conf" do
+  source 'tmux/tmux.conf'
+  owner node[:elephant][:username]
+  group node[:elephant][:group]
+end
