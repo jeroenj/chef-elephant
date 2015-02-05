649ddc70186510a1931401c2d9d249199055f7db
Jeroen Jacobs <git@jeroenj.be>
2015-02-05 21:03:13 +0100

    [git] Adds pre-commit hook to git templates and use it in git config

--- recipes/git.rb
+++ recipes/git.rb
@@ -4,6 +4,18 @@ cookbook_file "#{ENV['HOME']}/.gitignore" do
   group node[:elephant][:group]
 end
 
+elephant_recursive_directory "#{ENV['HOME']}/.git-templates/hooks" do
+  owner node[:elephant][:username]
+  group node[:elephant][:group]
+end
+
+cookbook_file "#{ENV['HOME']}/.git-templates/hooks/pre-commit" do
+  source 'git/pre-commit'
+  mode 0755
+  owner node[:elephant][:username]
+  group node[:elephant][:group]
+end
+
 cookbook_file "#{ENV['HOME']}/.oh-my-zsh/custom/git.zsh" do
   source 'git/git.zsh'
   only_if { Dir.exists? "#{ENV['HOME']}/.oh-my-zsh/custom" }
