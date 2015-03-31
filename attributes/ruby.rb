f277417a5dcb9550a0a6986eb65f346ff44d14ba
Jeroen Jacobs <git@jeroenj.be>
2015-03-31 16:38:22 +0200

    [ruby] Updates bundler to v1.9.2

--- attributes/ruby.rb
+++ attributes/ruby.rb
@@ -1,5 +1,5 @@
 default[:elephant][:ruby][:gems] = [
-  { :name => 'bundler', :version => '~> 1.8.3' },
+  { :name => 'bundler', :version => '~> 1.9.2' },
   { :name => 'bundler-audit', :version => '~> 0.3.1' },
   { :name => 'cheat' },
   { :name => 'explain_shell' },
