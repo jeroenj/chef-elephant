248bec6bccaa3a63c936e7a04d32988f0f73fcda
Jeroen Jacobs <git@jeroenj.be>
2015-06-04 15:18:25 +0200

    [brew] Set up taps before installing formulas

--- recipes/brew.rb
+++ recipes/brew.rb
@@ -1,5 +1,4 @@
 include_recipe 'homebrew'
-include_recipe 'homebrew::install_formulas'
 
 homebrew_tap 'homebrew/dupes'
 homebrew_tap 'caskroom/cask'
@@ -11,3 +10,5 @@ elephant_recursive_directory '/usr/local/var/log' do
   owner node[:elephant][:username]
   group 'admin'
 end
+
+include_recipe 'homebrew::install_formulas'
