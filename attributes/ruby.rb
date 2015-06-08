db2dd1bcaa7846f442f247b86f41cc0e3f7c63c9
Jeroen Jacobs <git@jeroenj.be>
2015-06-08 15:36:19 +0200

    [ruby] Installs bundler ~> 1.10.3

--- attributes/ruby.rb
+++ attributes/ruby.rb
@@ -1,5 +1,5 @@
 default[:elephant][:ruby][:gems] = [
-  { :name => 'bundler', :version => '~> 1.9.2' },
+  { :name => 'bundler', :version => '~> 1.10.3' },
   { :name => 'bundler-audit', :version => '~> 0.3.1' },
   { :name => 'cheat' },
   { :name => 'explain_shell' },
