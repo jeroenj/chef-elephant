e79530682c055edba381ab5c80c651eb4dd8f814
Jeroen Jacobs <git@jeroenj.be>
2015-06-08 15:36:50 +0200

    [chruby] Removes 1.8.7, 2.1.6 and 2.2.0 from default installed rubies

--- attributes/chruby.rb
+++ attributes/chruby.rb
@@ -2,10 +2,7 @@ default[:elephant][:chruby] = {
   :path => '/opt/rubies',
   :rubies => %w[
     2.2.2
-    2.2.0
-    2.1.6
     2.0.0-p598
-    1.8.7-p374
   ],
   :default => '2.2.2',
   :gems => [
