52887c35f0b99e27a7d40bdc8d8900f96fccbcc9
Jeroen Jacobs <git@jeroenj.be>
2015-06-23 17:08:33 +0200

    [postgresql] Fixes launchd symlink

--- recipes/postgresql.rb
+++ recipes/postgresql.rb
@@ -1,7 +1,7 @@
 package 'postgresql'
 
 link "#{ENV['HOME']}/Library/LaunchAgents/homebrew.mxcl.postgresql.plist" do
-  to '/usr/local/opt/mariadb/homebrew.mxcl.postgresql.plist'
+  to '/usr/local/opt/postgresql/homebrew.mxcl.postgresql.plist'
   owner node[:elephant][:username]
   group node[:elephant][:group]
 end
