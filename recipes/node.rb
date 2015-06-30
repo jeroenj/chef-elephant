d7cdac092746b5a31946f57ec7a934f020ba30ff
Jeroen Jacobs <git@jeroenj.be>
2015-06-30 12:27:48 +0200

    [node] Fixes not_if check

--- recipes/node.rb
+++ recipes/node.rb
@@ -1,12 +1,9 @@
 package 'node'
 
 node[:elephant][:node][:packages].each do |package|
-  # A bit nasty: coffee-script has coffee as an executable
-  executable = package.split('-').first
-
   execute "npm install -g #{package}" do
     user node[:elephant][:username]
     group node[:elephant][:group]
-    not_if { ::File.exists? "/usr/local/share/npm/bin/#{executable}" }
+    not_if "npm list #{package} version -g --silent > /dev/null"
   end
 end
