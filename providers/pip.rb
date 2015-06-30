e60abf2eef090a26d4905a4ace4c9d19f54d251c
Jeroen Jacobs <git@jeroenj.be>
2015-06-30 12:02:51 +0200

    [python] Enhances not_if check

--- providers/pip.rb
+++ providers/pip.rb
@@ -3,6 +3,6 @@ action :install do
     command "pip install #{new_resource.name}"
     user node[:elephant][:username]
     group node[:elephant][:group]
-    not_if { `pip show #{new_resource.name}` =~ /#{new_resource.name}/i }
+    not_if "pip show #{new_resource.name} --quiet"
   end
 end
