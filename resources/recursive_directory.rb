5b518bb75dde6a2bff78ffa1e66bb69797bac3a7
Jeroen Jacobs <git@jeroenj.be>
2015-10-22 15:23:00 +0200

    Don't try to create /usr/local in the recursive_directory LWRP
    Since OSX 10.11 /usr is write protected (even for root) causing
    recursive directories in /usr/local to fail. As far as I know OSX does
    not create /usr/local by default so this will need to be done manually
    before running this cookbook.

--- resources/recursive_directory.rb
+++ resources/recursive_directory.rb
@@ -9,7 +9,7 @@ attribute :parent, :kind_of => String
 def initialize(name, run_context = nil)
   super
   @parent ||= case path
-  when /\/usr.*/ then '/usr'
+  when /\/usr\/local\/.*/ then '/usr/local'
   when /#{ENV['HOME']}\/Library\/Application Support\/.*/ then "#{ENV['HOME']}/Library/Application Support"
   when /#{ENV['HOME']}\/Library\/Preferences\/.*/ then "#{ENV['HOME']}/Library/Preferences"
   when /#{ENV['HOME']}\/.*/ then ENV['HOME']
