08dd5466cd279d382bd4f8f3dabfbb0129656ae8
Jeroen Jacobs <git@jeroenj.be>
2015-12-23 15:47:46 +0100

    [chruby] Installs bundler ~> 1.11.2

--- attributes/chruby.rb
+++ attributes/chruby.rb
@@ -8,7 +8,7 @@ default[:elephant][:chruby] = {
   ),
   default: '2.2.3',
   gems: [
-    { name: 'bundler', version: '~> 1.11.0' },
+    { name: 'bundler', version: '~> 1.11.2' },
     { name: 'bundler-audit', version: '~> 0.4.0' },
     { name: 'cheat' },
     { name: 'explain_shell' },
